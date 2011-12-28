3c485a97e59eb3c37ac86a756276bc3f73122298
Gino Miceli <gino.miceli@fao.org>
2011-12-28 14:39:00 +0100

    Work in progress

---
+++ collect-core/design/database/before-init-postgresql.sql
@@ -0,0 +1,11 @@
+CREATE SEQUENCE collect.data_id_seq
+GO
+
+CREATE SEQUENCE collect.record_id_seq
+GO
+
+CREATE SEQUENCE collect.schema_definition_id_seq
+GO
+
+CREATE SEQUENCE collect.survey_id_seq
+GO
