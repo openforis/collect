31b6d7bd23059ddd86c167397e6e2cb960adb049
riccist <stefano.ricci@fao.org>
2012-01-06 11:10:39 +0100

    added method load (by name)

--- collect-core/src/main/java/org/openforis/collect/manager/SurveyManager.java
+++ collect-core/src/main/java/org/openforis/collect/manager/SurveyManager.java
@@ -3,10 +3,25 @@
  */
 package org.openforis.collect.manager;
 
+import java.util.List;
+
+import org.openforis.collect.persistence.SurveyDAO;
+import org.openforis.idm.metamodel.Survey;
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * @author M. Togna
  * 
  */
 public class SurveyManager {
 //	private EntityManager entityManager;
+	
+	@Autowired
+	private SurveyDAO surveyDAO;
+	
+	public Survey load(String name) {
+		Survey survey = surveyDAO.load(name);
+		return survey;
+	}
+
 }
