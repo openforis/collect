8f14aa69eba60e109c97729ed69b5950c6a85d98
riccist <stefano.ricci@fao.org>
2012-01-17 15:46:27 +0100

    added methods delete and deleteRecord

--- collect-core/src/main/java/org/openforis/collect/persistence/RecordDAO.java
+++ collect-core/src/main/java/org/openforis/collect/persistence/RecordDAO.java
@@ -56,6 +56,12 @@ public class RecordDAO extends CollectDAO {
 	}
 
 	@Transactional
+	public void delete(CollectRecord record) {
+		deleteData(record.getId());
+		deleteRecord(record);
+	}
+	
+	@Transactional
 	public int getCountRecords(EntityDefinition rootEntityDefinition, String filter) {
 		Factory jf = getJooqFactory();
 		Record r = jf.select(Factory.count()).from(RECORD).where(RECORD.ROOT_ENTITY_ID.equal(rootEntityDefinition.getId())).fetchOne();
@@ -73,11 +79,6 @@ public class RecordDAO extends CollectDAO {
 		List<Record> records = selectQuery.fetch();
 
 		List<RecordSummary> result = recordSummaryLoader.parseResult(records);
-		
-		if(LOG.isDebugEnabled()) {
-			String sql = selectQuery.getSQL();
-			LOG.debug(sql);
-		}
 		
 		return result;
 	}
@@ -185,6 +186,15 @@ public class RecordDAO extends CollectDAO {
 				.set(RECORD.DATE_MODIFIED, toTimestamp(record.getModifiedDate()))
 				// .set(RECORD.MODIFIED_BY, record.getModifiedBy())
 				.set(RECORD.MODEL_VERSION, record.getVersion().getName()).set(RECORD.STEP, record.getStep().getStepNumber()).where(RECORD.ID.equal(recordId)).execute();
+	}
+	
+	private void deleteRecord(CollectRecord record) {
+		Integer recordId = record.getId();
+		if (recordId == null) {
+			throw new IllegalArgumentException("Cannot update unsaved record");
+		}
+		Factory jf = getJooqFactory();
+		jf.delete(RECORD).where(RECORD.ID.equal(recordId)).execute();
 	}
 
 	private void deleteData(int recordId) {
