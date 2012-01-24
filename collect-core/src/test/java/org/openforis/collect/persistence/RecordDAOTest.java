18bd1e9e8973bbd1ad70d6bd0f1c85553e2b1b05
riccist <stefano.ricci@fao.org>
2012-01-24 16:34:55 +0100

    added logo table

---
+++ collect-core/src/test/java/org/openforis/collect/persistence/RecordDAOTest.java
@@ -0,0 +1,212 @@
+package org.openforis.collect.persistence;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openforis.collect.model.CollectAttributeMetadata;
+import org.openforis.collect.model.CollectRecord;
+import org.openforis.collect.model.CollectRecord.Step;
+import org.openforis.collect.model.RecordSummary;
+import org.openforis.collect.model.UIConfiguration.UIConfigurationAdapter;
+import org.openforis.idm.metamodel.EntityDefinition;
+import org.openforis.idm.metamodel.Survey;
+import org.openforis.idm.metamodel.xml.BindingContext;
+import org.openforis.idm.metamodel.xml.InvalidIdmlException;
+import org.openforis.idm.model.AlphanumericCode;
+import org.openforis.idm.model.Coordinate;
+import org.openforis.idm.model.Date;
+import org.openforis.idm.model.Entity;
+import org.openforis.idm.model.NumericCode;
+import org.openforis.idm.model.Time;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+//@RunWith( SpringJUnit4ClassRunner.class )
+//@ContextConfiguration( locations = {"classpath:test-context.xml"} )
+//@TransactionConfiguration(defaultRollback=false)
+//@Transactional
+public class RecordDAOTest {
+	private final Log log = LogFactory.getLog(RecordDAOTest.class);
+	
+	private BindingContext bindingContext;
+	
+	@Autowired
+	protected SurveyDAO surveyDao;
+	
+	@Autowired
+	protected RecordDAO recordDao;
+	
+	private Survey survey;
+	
+	private List<CollectRecord> sampleRecords;
+	
+//	@Before
+	public void beforeTest(){
+		surveyDao.loadAll();
+		bindingContext = new BindingContext();
+		UIConfigurationAdapter configurationAdapter = new UIConfigurationAdapter();
+		bindingContext.setConfigurationAdapter(configurationAdapter);
+		
+		// LOAD MODEL
+		survey = surveyDao.load("archenland1");
+		
+		createAndSaveSampleRecords();
+	}
+	
+	//@After
+	public void afterTest() {
+		//delete sample records
+		for (CollectRecord record : sampleRecords) {
+			recordDao.delete(record);
+		}
+	}
+	
+	/**
+	 * 
+	 * Creates 9 sample records with different id and different number of plots.
+	 * 
+	 */
+	private void createAndSaveSampleRecords() {
+		int numberOfClusters = 1000;
+		sampleRecords = new ArrayList<CollectRecord>();
+		
+		//create and save clusters
+		for (int i = 1; i <= numberOfClusters ; i++) {
+			CollectRecord record = createRecord(survey, i);
+			sampleRecords.add(record);
+			recordDao.saveOrUpdate(record);
+		}
+	}
+	
+//	@Test
+	public void testLoadRecordSummariesOrderedByClusterId() throws IOException, SurveyImportException, DataInconsistencyException, InvalidIdmlException, NonexistentIdException  {
+		EntityDefinition rootEntity = survey.getSchema().getRootEntityDefinitions().get(0);
+
+		//load record summaries
+		int offset = 0;
+		int maxNumberOfRecords = 5;
+		String orderByFieldName = "key_id";
+		String filter = null;
+		List<EntityDefinition> countInSummaryListEntityDefinitions = new ArrayList<EntityDefinition>();
+		EntityDefinition plotEntity = (EntityDefinition) rootEntity.getChildDefinition("plot");
+		countInSummaryListEntityDefinitions.add(plotEntity);
+		List<RecordSummary> list = this.recordDao.loadRecordSummaries(rootEntity, countInSummaryListEntityDefinitions, offset, maxNumberOfRecords, orderByFieldName, filter);
+
+		assertNotNull(list);
+		assertEquals(maxNumberOfRecords, list.size());
+		
+		//test first record of the page
+		RecordSummary sampleRecordSummary;
+		Map<String, String> rootEntityKeys;
+		
+		sampleRecordSummary = list.get(0);
+		rootEntityKeys = sampleRecordSummary.getRootEntityKeys();
+		
+		assertEquals("1", rootEntityKeys.get("id"));
+		
+		//test last record of the page
+		sampleRecordSummary = list.get(4);
+		rootEntityKeys = sampleRecordSummary.getRootEntityKeys();
+		
+		assertEquals("5", rootEntityKeys.get("id"));
+	}
+
+	//@Test
+	public void testLoadRecordSummariesOrderedByPlotCount() throws IOException, SurveyImportException, DataInconsistencyException, InvalidIdmlException, NonexistentIdException  {
+		EntityDefinition rootEntity = survey.getSchema().getRootEntityDefinitions().get(0);
+
+		//load record summaries
+		int offset = 0;
+		int maxNumberOfRecords = 5;
+		String orderByFieldName = "count_plot";
+		String filter = null;
+		
+		List<EntityDefinition> countInSummaryListEntityDefinitions = new ArrayList<EntityDefinition>();
+		EntityDefinition plotEntity = (EntityDefinition) rootEntity.getChildDefinition("plot");
+		countInSummaryListEntityDefinitions.add(plotEntity);
+		List<RecordSummary> list = this.recordDao.loadRecordSummaries(rootEntity, countInSummaryListEntityDefinitions, offset, maxNumberOfRecords, orderByFieldName, filter);
+		
+		assertNotNull(list);
+		assertEquals(maxNumberOfRecords, list.size());
+
+		//test first record of the page
+		{
+			RecordSummary sampleRecordSummary = list.get(0);
+			Map<String, Integer> entityCounts = sampleRecordSummary.getEntityCounts();
+			
+			assertEquals(new Integer(1), entityCounts.get("plot"));
+		}
+		//test last record of the page
+		{
+			RecordSummary sampleRecordSummary = list.get(4);
+			Map<String, Integer> entityCounts = sampleRecordSummary.getEntityCounts();
+		
+			assertEquals(new Integer(5), entityCounts.get("plot"));
+		}
+	}
+
+	private CollectRecord createRecord(Survey survey, int sequenceNumber) {
+		int skippedCount = new Double(Math.ceil((double) (Math.random() * 40))).intValue();
+		int missingCount = new Double(Math.ceil((double) (Math.random() * 50))).intValue();
+		int errorsCount = new Double(Math.ceil((double) (Math.random() * 10))).intValue();
+		int warningsCount = new Double(Math.ceil((double) (Math.random() * 30))).intValue();
+		int numberOfPlots = new Double(Math.ceil((double) (Math.random() * 20))).intValue();
+		int numberOfTrees = new Double(Math.ceil((double) (Math.random() * 30))).intValue();;
+		CollectRecord record = new CollectRecord(survey, "cluster", "2.0");
+		record.setCreationDate(new GregorianCalendar(2011, 0, sequenceNumber, 8, 30).getTime());
+		//record.setCreatedBy("DAOIntegrationTest");
+		record.setStep(Step.ENTRY);
+		record.setModifiedDate(new GregorianCalendar(2011, 1, sequenceNumber, 8, 30).getTime());
+		record.setSkipped(skippedCount);
+		record.setMissing(missingCount);
+		record.setErrors(errorsCount);
+		record.setWarnings(warningsCount);
+		Entity cluster = record.getRootEntity();
+		cluster.addValue("id", new AlphanumericCode(Integer.toString(sequenceNumber)));
+		cluster.addValue("gps_realtime", Boolean.TRUE);
+		cluster.addValue("region", new NumericCode(001));
+		cluster.addValue("district", new NumericCode(002));
+		cluster.addValue("vehicle_location", new Coordinate(432423423l, 4324324l,"srs"));
+		cluster.addValue("gps_model", "TomTom 1.232");
+		{
+			Entity ts = cluster.addEntity("time_study");
+			ts.addValue("date", new Date(2011,2,14));
+			ts.addValue("start_time", new Time(8,15));
+			ts.addValue("end_time", new Time(15,29));
+		}
+		{
+			Entity ts = cluster.addEntity("time_study");
+			ts.addValue("date", new Date(2011,2,15));
+			ts.addValue("start_time", new Time(8,32));
+			ts.addValue("end_time", new Time(11,20));
+		}
+		for (int i = 0; i < numberOfPlots; i++) {
+			Entity plot = cluster.addEntity("plot");
+			plot.addValue("no", new AlphanumericCode(Integer.toString(i + 1)));
+			
+			for (int j = 0; j < numberOfTrees; j++) {
+				Entity tree = plot.addEntity("tree");
+				tree.addValue("dbh", 54.2);
+				tree.addValue("total_height", 2.0);
+				tree.addValue("bole_height", (Double) null).setMetadata(new CollectAttributeMetadata('*',null,"No value specified"));
+			}
+		}
+		return record;
+	}
+
+}
