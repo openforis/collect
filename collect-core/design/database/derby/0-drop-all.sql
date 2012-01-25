6ad7a471b40c603afff0f1b2ceb2e3566e892419
Gino Miceli <gino.miceli@fao.org>
2012-01-25 09:44:00 +0100

    Moving SQL to src

---
+++ collect-core/design/database/derby/0-drop-all.sql
@@ -0,0 +1,29 @@
+DROP SEQUENCE collect.data_id_seq RESTRICT
+GO
+
+DROP SEQUENCE collect.record_id_seq RESTRICT
+GO
+
+DROP SEQUENCE collect.schema_definition_id_seq RESTRICT
+GO
+
+DROP SEQUENCE collect.survey_id_seq RESTRICT
+GO
+
+DROP SEQUENCE collect.taxonomy_id_seq RESTRICT
+GO
+
+DROP SEQUENCE collect.taxon_id_seq RESTRICT
+GO
+
+DROP SEQUENCE collect.taxon_name_id_seq RESTRICT
+GO
+
+DROP SEQUENCE collect.user_id_seq	 RESTRICT
+GO
+
+DROP SEQUENCE collect.user_role_id_seq RESTRICT
+GO
+
+DROP SCHEMA collect RESTRICT
+GO
---
+++ collect-core/design/database/postgresql/0-drop-all.sql
@@ -0,0 +1,29 @@
+DROP SEQUENCE collect.data_id_seq 
+GO
+
+DROP SEQUENCE collect.record_id_seq 
+GO
+
+DROP SEQUENCE collect.schema_definition_id_seq 
+GO
+
+DROP SEQUENCE collect.survey_id_seq 
+GO
+
+DROP SEQUENCE collect.taxonomy_id_seq 
+GO
+
+DROP SEQUENCE collect.taxon_id_seq 
+GO
+
+DROP SEQUENCE collect.taxon_name_id_seq 
+GO
+
+DROP SEQUENCE collect.user_id_seq	 
+GO
+
+DROP SEQUENCE collect.user_role_id_seq 
+GO
+
+DROP SCHEMA collect 
+GO
