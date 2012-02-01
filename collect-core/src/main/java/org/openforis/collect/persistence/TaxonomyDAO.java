3e82ea98021aeb1411b72c451ef91da2459cba06
Gino Miceli <gino.miceli@fao.org>
2012-02-01 18:20:59 +0100

    Added TableMapper to simply DAOs

--- collect-core/src/main/java/org/openforis/collect/persistence/TaxonomyDAO.java
+++ collect-core/src/main/java/org/openforis/collect/persistence/TaxonomyDAO.java
@@ -3,66 +3,62 @@ package org.openforis.collect.persistence;
 import static org.openforis.collect.persistence.jooq.Sequences.TAXONOMY_ID_SEQ;
 import static org.openforis.collect.persistence.jooq.tables.Taxonomy.TAXONOMY;
 
-import org.jooq.InsertQuery;
 import org.jooq.Record;
 import org.jooq.UpdatableRecord;
-import org.jooq.UpdateQuery;
-import org.jooq.impl.Factory;
 import org.openforis.collect.model.species.Taxonomy;
-import org.openforis.collect.persistence.jooq.tables.records.TaxonomyRecord;
 
 /**
  * @author G. Miceli
  */
 public class TaxonomyDAO extends CollectDAO {
 
+	private TaxonomyMapper mapper;
+	
+	public TaxonomyDAO() {
+		mapper = new TaxonomyMapper();
+	}
+	
 	public Taxonomy load(int id) {
-		Record r =  getJooqFactory()
-				.select()
-				.where(TAXONOMY.ID.equal(id))
-				.fetchOne();
-		Taxonomy t = new Taxonomy();
-		t.setId(r.getValue(TAXONOMY.ID));
-		t.setName(r.getValue(TAXONOMY.NAME));
+		Record r = mapper.selectQuery(getJooqFactory(), id).fetchOne();
+		Taxonomy t = mapper.fromRecord(r);
 		return t;
 	}
 	
-	@SuppressWarnings({"rawtypes", "unchecked"})
 	public void insert(Taxonomy t) {
-		Factory jf = getJooqFactory();
-		int nextId = jf.nextval(TAXONOMY_ID_SEQ).intValue();
-		t.setId(nextId);
-		
-		UpdatableRecord r = toRecord(t);
-		
-		InsertQuery insert = jf.insertQuery(r.getTable());
-		insert.setRecord(r);
-		insert.execute();
+		mapper.insertQuery(getJooqFactory(), t).execute();
 	}
 
 	
-	@SuppressWarnings({"rawtypes", "unchecked"})
 	public void update(Taxonomy t) {	
-		UpdatableRecord r = toRecord(t);
-		
-		UpdateQuery update = getJooqFactory().updateQuery(r.getTable());
-		update.setRecord(r);
-		update.execute();
+		mapper.updateQuery(getJooqFactory(), t).execute();
 	}
 
 	public void delete(int id) {
-		getJooqFactory()
-			.delete(TAXONOMY)
-			.where(TAXONOMY.ID.equal(id))
-			.execute();
-	}
-	
-	private UpdatableRecord<?> toRecord(Taxonomy t) {
-		UpdatableRecord<?> r = getJooqFactory().newRecord(TAXONOMY);
-		r.setValue(TAXONOMY.ID, t.getId());
-		r.setValue(TAXONOMY.NAME, t.getName());
-		r.setValue(TAXONOMY.METADATA, "Not yet implemented");
-		return r;
+		mapper.deleteQuery(getJooqFactory(), id).execute();
 	}
 
+	public class TaxonomyMapper extends TableMapper<Taxonomy> {
+
+		public TaxonomyMapper() {
+			super(TAXONOMY.ID, TAXONOMY_ID_SEQ, Taxonomy.class);
+		}
+
+		@Override
+		public void fromRecord(Record r, Taxonomy t) {
+			t.setId(r.getValue(TAXONOMY.ID));
+			t.setName(r.getValue(TAXONOMY.NAME));
+		}
+		
+		@Override
+		public void toRecord(Taxonomy t, UpdatableRecord<?> r) {
+			r.setValue(TAXONOMY.ID, t.getId());
+			r.setValue(TAXONOMY.NAME, t.getName());
+			r.setValue(TAXONOMY.METADATA, "Not yet implemented");
+		}
+
+		@Override
+		protected void setId(Taxonomy taxonomy, int id) {
+			taxonomy.setId(id);
+		}
+	}
 }
