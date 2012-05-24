f589f474cf67d501a518ef8bcd87862887272160
Stefano Ricci <stefano.ricci@fao.org>
2012-05-24 17:27:59 +0200

    OFC-580 In create and drop table scripts, replace GO with ;

--- collect-core/src/main/resources/org/openforis/collect/sql/postgresql/create-schema.sql
+++ collect-core/src/main/resources/org/openforis/collect/sql/postgresql/create-schema.sql
@@ -1,29 +1,12 @@
-CREATE SCHEMA "collect"
-GO
-
-CREATE SEQUENCE "collect"."ofc_record_id_seq"
-GO
-
-CREATE SEQUENCE "collect"."ofc_schema_definition_id_seq"
-GO
-
-CREATE SEQUENCE "collect"."ofc_survey_id_seq"
-GO
-
-CREATE SEQUENCE "collect"."ofc_taxonomy_id_seq"
-GO
-
-CREATE SEQUENCE "collect"."ofc_taxon_id_seq"
-GO
-
-CREATE SEQUENCE "collect"."ofc_taxon_vernacular_name_id_seq"
-GO
-
-CREATE SEQUENCE "collect"."ofc_user_id_seq"	
-GO
-
-CREATE SEQUENCE "collect"."ofc_user_role_id_seq"
-GO
+CREATE SCHEMA "collect";
+CREATE SEQUENCE "collect"."ofc_record_id_seq";
+CREATE SEQUENCE "collect"."ofc_schema_definition_id_seq";
+CREATE SEQUENCE "collect"."ofc_survey_id_seq";
+CREATE SEQUENCE "collect"."ofc_taxonomy_id_seq";
+CREATE SEQUENCE "collect"."ofc_taxon_id_seq";
+CREATE SEQUENCE "collect"."ofc_taxon_vernacular_name_id_seq";
+CREATE SEQUENCE "collect"."ofc_user_id_seq"	;
+CREATE SEQUENCE "collect"."ofc_user_role_id_seq";
 
 ----------------------------
 --- BEGIN GENERATED CODE ---
@@ -33,14 +16,12 @@ CREATE TABLE "collect"."ofc_config"  (
 	"name" 	varchar(25) NOT NULL,
 	"value"	varchar(255) NOT NULL,
 	PRIMARY KEY("name")
-)
-GO
+);
 CREATE TABLE "collect"."ofc_logo"  ( 
 	"pos"  	integer NOT NULL,
 	"image"	bytea NOT NULL,
 	PRIMARY KEY("pos")
-)
-GO
+);
 CREATE TABLE "collect"."ofc_record"  ( 
 	"id"                       	integer NOT NULL,
 	"root_entity_definition_id"	integer NOT NULL,
@@ -66,23 +47,20 @@ CREATE TABLE "collect"."ofc_record"  (
 	"data1"                    	bytea NULL,
 	"data2"                    	bytea NULL,
 	PRIMARY KEY("id")
-)
-GO
+);
 CREATE TABLE "collect"."ofc_schema_definition"  ( 
 	"id"       	integer NOT NULL,
 	"survey_id"	integer NOT NULL,
 	"path"     	varchar(255) NULL,
 	PRIMARY KEY("id")
-)
-GO
+);
 CREATE TABLE "collect"."ofc_survey"  ( 
 	"id"  	integer NOT NULL,
 	"name"	varchar(255) NOT NULL,
 	"uri" 	varchar(255) NULL,
 	"idml"	text NOT NULL,
 	PRIMARY KEY("id")
-)
-GO
+);
 CREATE TABLE "collect"."ofc_taxon"  ( 
 	"id"             	integer NOT NULL,
 	"taxon_id"       	integer NOT NULL,
@@ -93,8 +71,7 @@ CREATE TABLE "collect"."ofc_taxon"  (
 	"step"           	integer NOT NULL,
 	"parent_id"      	integer NULL,
 	PRIMARY KEY("id")
-)
-GO
+);
 CREATE TABLE "collect"."ofc_taxon_vernacular_name"  ( 
 	"id"              	integer NOT NULL,
 	"vernacular_name" 	varchar(255) NULL,
@@ -103,88 +80,71 @@ CREATE TABLE "collect"."ofc_taxon_vernacular_name"  (
 	"taxon_id"        	integer NULL,
 	"step"            	integer NOT NULL,
 	PRIMARY KEY("id")
-)
-GO
-COMMENT ON COLUMN "collect"."ofc_taxon_vernacular_name"."language_variety" IS 'Dialect, lect, sublanguage or other'
-GO
+);
+COMMENT ON COLUMN "collect"."ofc_taxon_vernacular_name"."language_variety" IS 'Dialect, lect, sublanguage or other';
 CREATE TABLE "collect"."ofc_taxonomy"  ( 
 	"id"      	integer NOT NULL,
 	"name"    	varchar(255) NOT NULL,
 	"metadata"	text NOT NULL,
 	PRIMARY KEY("id")
-)
-GO
+);
 CREATE TABLE "collect"."ofc_user"  ( 
 	"id"      	integer NOT NULL,
 	"username"	varchar(255) NOT NULL,
 	"password"	varchar(255) NOT NULL,
 	"enabled" 	char(1) NOT NULL DEFAULT 'Y',
 	PRIMARY KEY("id")
-)
-GO
+);
 CREATE TABLE "collect"."ofc_user_role"  ( 
 	"id"     	integer NOT NULL,
 	"user_id"	integer NOT NULL,
 	"role"   	varchar(256) NULL,
 	PRIMARY KEY("id")
-)
-GO
+);
 ALTER TABLE "collect"."ofc_survey"
 	ADD CONSTRAINT "ofc_survey_name_key"
-	UNIQUE ("name")
-GO
+	UNIQUE ("name");
 ALTER TABLE "collect"."ofc_survey"
 	ADD CONSTRAINT "ofc_survey_uri_key"
-	UNIQUE ("uri")
-GO
+	UNIQUE ("uri");
 ALTER TABLE "collect"."ofc_taxon"
 	ADD CONSTRAINT "ofc_taxon_id_key"
-	UNIQUE ("taxon_id", "taxonomy_id")
-GO
+	UNIQUE ("taxon_id", "taxonomy_id");
 ALTER TABLE "collect"."ofc_taxonomy"
 	ADD CONSTRAINT "ofc_taxonomy_name_key"
-	UNIQUE ("name")
-GO
+	UNIQUE ("name");
 ALTER TABLE "collect"."ofc_record"
 	ADD CONSTRAINT "ofc_record_root_entity_definition_fkey"
 	FOREIGN KEY("root_entity_definition_id")
-	REFERENCES "collect"."ofc_schema_definition"("id")
-GO
+	REFERENCES "collect"."ofc_schema_definition"("id");
 ALTER TABLE "collect"."ofc_schema_definition"
 	ADD CONSTRAINT "ofc_schema_definition_survey_fkey"
 	FOREIGN KEY("survey_id")
-	REFERENCES "collect"."ofc_survey"("id")
-GO
+	REFERENCES "collect"."ofc_survey"("id");
 ALTER TABLE "collect"."ofc_taxon_vernacular_name"
 	ADD CONSTRAINT "ofc_taxon_vernacular_name_taxon_fkey"
 	FOREIGN KEY("taxon_id")
-	REFERENCES "collect"."ofc_taxon"("id")
-GO
+	REFERENCES "collect"."ofc_taxon"("id");
 ALTER TABLE "collect"."ofc_taxon"
 	ADD CONSTRAINT "ofc_taxon_parent_fkey"
 	FOREIGN KEY("parent_id")
-	REFERENCES "collect"."ofc_taxon"("id")
-GO
+	REFERENCES "collect"."ofc_taxon"("id");
 ALTER TABLE "collect"."ofc_taxon"
 	ADD CONSTRAINT "ofc_taxon_taxonomy_fkey"
 	FOREIGN KEY("taxonomy_id")
-	REFERENCES "collect"."ofc_taxonomy"("id")
-GO
+	REFERENCES "collect"."ofc_taxonomy"("id");
 ALTER TABLE "collect"."ofc_user_role"
 	ADD CONSTRAINT "ofc_user_user_role_fkey"
 	FOREIGN KEY("user_id")
-	REFERENCES "collect"."ofc_user"("id")
-GO
+	REFERENCES "collect"."ofc_user"("id");
 ALTER TABLE "collect"."ofc_record"
 	ADD CONSTRAINT "ofc_record_created_by_user_fkey"
 	FOREIGN KEY("created_by_id")
-	REFERENCES "collect"."ofc_user"("id")
-GO
+	REFERENCES "collect"."ofc_user"("id");
 ALTER TABLE "collect"."ofc_record"
 	ADD CONSTRAINT "ofc_record_modified_by_user_fkey"
 	FOREIGN KEY("modified_by_id")
-	REFERENCES "collect"."ofc_user"("id")
-GO
+	REFERENCES "collect"."ofc_user"("id");
 --------------------------
 --- END GENERATED CODE ---
 --------------------------
@@ -193,8 +153,7 @@ GO
 ----------------------------
 --- BEGIN DEFAULT VALUES ---
 ----------------------------
-INSERT INTO "collect"."ofc_config" ("name", "value") VALUES ('upload_path', '/home/openforis/collect-upload')
-GO
+INSERT INTO "collect"."ofc_config" ("name", "value") VALUES ('upload_path', '/home/openforis/collect-upload');
 --------------------------
 --- END DEFAULT VALUES ---
 --------------------------
--- collect-core/src/main/resources/org/openforis/collect/sql/postgresql/drop-schema.sql
+++ collect-core/src/main/resources/org/openforis/collect/sql/postgresql/drop-schema.sql
@@ -1,91 +1,53 @@
-DROP SEQUENCE "collect"."ofc_record_id_seq" 
-GO
-
-DROP SEQUENCE "collect"."ofc_schema_definition_id_seq" 
-GO
-
-DROP SEQUENCE "collect"."ofc_survey_id_seq" 
-GO
-
-DROP SEQUENCE "collect"."ofc_taxonomy_id_seq" 
-GO
-
-DROP SEQUENCE "collect"."ofc_taxon_id_seq" 
-GO
-
-DROP SEQUENCE "collect"."ofc_taxon_vernacular_name_id_seq" 
-GO
-
-DROP SEQUENCE "collect"."ofc_user_id_seq"	 
-GO
-
-DROP SEQUENCE "collect"."ofc_user_role_id_seq" 
-GO
+DROP SEQUENCE "collect"."ofc_record_id_seq";
+DROP SEQUENCE "collect"."ofc_schema_definition_id_seq";
+DROP SEQUENCE "collect"."ofc_survey_id_seq";
+DROP SEQUENCE "collect"."ofc_taxonomy_id_seq";
+DROP SEQUENCE "collect"."ofc_taxon_id_seq";
+DROP SEQUENCE "collect"."ofc_taxon_vernacular_name_id_seq";
+DROP SEQUENCE "collect"."ofc_user_id_seq";
+DROP SEQUENCE "collect"."ofc_user_role_id_seq";
 
 ----------------------------
 --- BEGIN GENERATED CODE ---
 ----------------------------
 
 ALTER TABLE "collect"."ofc_record"
-	DROP CONSTRAINT "ofc_record_root_entity_definition_fkey" CASCADE 
-GO
+	DROP CONSTRAINT "ofc_record_root_entity_definition_fkey" CASCADE;
 ALTER TABLE "collect"."ofc_schema_definition"
-	DROP CONSTRAINT "ofc_schema_definition_survey_fkey" CASCADE 
-GO
+	DROP CONSTRAINT "ofc_schema_definition_survey_fkey" CASCADE;
 ALTER TABLE "collect"."ofc_taxon_vernacular_name"
-	DROP CONSTRAINT "ofc_taxon_vernacular_name_taxon_fkey" CASCADE 
-GO
+	DROP CONSTRAINT "ofc_taxon_vernacular_name_taxon_fkey" CASCADE;
 ALTER TABLE "collect"."ofc_taxon"
-	DROP CONSTRAINT "ofc_taxon_parent_fkey" CASCADE 
-GO
+	DROP CONSTRAINT "ofc_taxon_parent_fkey" CASCADE;
 ALTER TABLE "collect"."ofc_taxon"
-	DROP CONSTRAINT "ofc_taxon_taxonomy_fkey" CASCADE 
-GO
+	DROP CONSTRAINT "ofc_taxon_taxonomy_fkey" CASCADE;
 ALTER TABLE "collect"."ofc_user_role"
-	DROP CONSTRAINT "ofc_user_user_role_fkey" CASCADE 
-GO
+	DROP CONSTRAINT "ofc_user_user_role_fkey" CASCADE;
 ALTER TABLE "collect"."ofc_record"
-	DROP CONSTRAINT "ofc_record_created_by_user_fkey" CASCADE 
-GO
+	DROP CONSTRAINT "ofc_record_created_by_user_fkey" CASCADE;
 ALTER TABLE "collect"."ofc_record"
-	DROP CONSTRAINT "ofc_record_modified_by_user_fkey" CASCADE 
-GO
+	DROP CONSTRAINT "ofc_record_modified_by_user_fkey" CASCADE;
 ALTER TABLE "collect"."ofc_survey"
-	DROP CONSTRAINT "ofc_survey_name_key" CASCADE 
-GO
+	DROP CONSTRAINT "ofc_survey_name_key" CASCADE;
 ALTER TABLE "collect"."ofc_survey"
-	DROP CONSTRAINT "ofc_survey_uri_key" CASCADE 
-GO
+	DROP CONSTRAINT "ofc_survey_uri_key" CASCADE;
 ALTER TABLE "collect"."ofc_taxon"
-	DROP CONSTRAINT "ofc_taxon_id_key" CASCADE
-GO
+	DROP CONSTRAINT "ofc_taxon_id_key" CASCADE;
 ALTER TABLE "collect"."ofc_taxonomy"
-	DROP CONSTRAINT "ofc_taxonomy_name_key" CASCADE 
-GO
-DROP TABLE "collect"."ofc_config"
-GO
-DROP TABLE IF EXISTS "collect"."ofc_logo"
-GO
-DROP TABLE IF EXISTS "collect"."ofc_record"
-GO
-DROP TABLE IF EXISTS "collect"."ofc_schema_definition"
-GO
-DROP TABLE IF EXISTS "collect"."ofc_survey"
-GO
-DROP TABLE IF EXISTS "collect"."ofc_taxon"
-GO
-DROP TABLE IF EXISTS "collect"."ofc_taxon_vernacular_name"
-GO
-DROP TABLE IF EXISTS "collect"."ofc_taxonomy"
-GO
-DROP TABLE IF EXISTS "collect"."ofc_user"
-GO
-DROP TABLE IF EXISTS "collect"."ofc_user_role"
-GO
+	DROP CONSTRAINT "ofc_taxonomy_name_key" CASCADE;
+DROP TABLE "collect"."ofc_config";
+DROP TABLE IF EXISTS "collect"."ofc_logo";
+DROP TABLE IF EXISTS "collect"."ofc_record";
+DROP TABLE IF EXISTS "collect"."ofc_schema_definition";
+DROP TABLE IF EXISTS "collect"."ofc_survey";
+DROP TABLE IF EXISTS "collect"."ofc_taxon";
+DROP TABLE IF EXISTS "collect"."ofc_taxon_vernacular_name";
+DROP TABLE IF EXISTS "collect"."ofc_taxonomy";
+DROP TABLE IF EXISTS "collect"."ofc_user";
+DROP TABLE IF EXISTS "collect"."ofc_user_role";
 
 --------------------------
 --- END GENERATED CODE ---
 --------------------------
 
-DROP SCHEMA "collect"
-GO
+DROP SCHEMA "collect";
