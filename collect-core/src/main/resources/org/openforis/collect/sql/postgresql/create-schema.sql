ac8fddc5d49e557776970f8d13acb5f44de84798
Stefano Ricci <stefano.ricci@fao.org>
2012-08-31 16:04:19 +0200

    Code clean

--- collect-core/src/main/resources/org/openforis/collect/sql/postgresql/create-schema.sql
+++ collect-core/src/main/resources/org/openforis/collect/sql/postgresql/create-schema.sql
@@ -154,14 +154,11 @@ INSERT INTO "collect"."ofc_config" ("name", "value") VALUES
 	('upload_path', '/home/openforis/collect-upload'), 
 	('index_path', '/home/openforis/collect-index');
 
-INSERT INTO "collect"."ofc_user" ("id", "username", "password", "enabled")
-VALUES (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Y');
-ALTER SEQUENCE "collect"."ofc_user_id_seq" RESTART WITH 2;
+INSERT INTO collect.ofc_user(id, username, password ,enabled)
+VALUE (nextval('collect.ofc_user_id_seq'), 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Y');
 
-INSERT INTO "collect"."ofc_user_role" ("id", "user_id", "role") 
-VALUES (1, 1, 'ROLE_ADMIN');
-ALTER SEQUENCE "collect"."ofc_user_role_id_seq" RESTART WITH 2;
-
+INSERT INTO collect.ofc_user_role(id, user_id, role)
+VALUES (nextval('collect.ofc_user_role_id_seq'), currval('collect.ofc_user_id_seq'), 'ROLE_ADMIN');
 
 --------------------------
 --- END DEFAULT VALUES ---
