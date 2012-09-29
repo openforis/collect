0367317db0d7651bddb1acf74ab82263348868d6
Stefano Ricci <stefano.ricci@fao.org>
2012-09-29 15:59:21 +0200

    OFC-726 Check node definitions name uniqueness

--- collect-server/src/main/java/org/openforis/collect/designer/form/validator/NodeDefinitionFormValidator.java
+++ collect-server/src/main/java/org/openforis/collect/designer/form/validator/NodeDefinitionFormValidator.java
@@ -1,8 +1,12 @@
 package org.openforis.collect.designer.form.validator;
 
+import java.util.Collection;
+
+import org.apache.commons.lang3.StringUtils;
 import org.openforis.collect.designer.viewmodel.SurveySchemaEditVM;
 import org.openforis.idm.metamodel.EntityDefinition;
 import org.openforis.idm.metamodel.NodeDefinition;
+import org.openforis.idm.metamodel.NodeDefinitionVisitor;
 import org.openforis.idm.metamodel.Schema;
 import org.zkoss.bind.BindContext;
 import org.zkoss.bind.ValidationContext;
@@ -15,6 +19,8 @@ import org.zkoss.zk.ui.Component;
  *
  */
 public class NodeDefinitionFormValidator extends FormValidator {
+
+	private static final String VM_ID_ATTRIBUTE = "$VM_ID$";
 
 	protected static final String NAME_ALREADY_DEFINED_MESSAGE_KEY = "survey.schema.node.validation.name_already_defined";
 	
@@ -35,7 +41,7 @@ public class NodeDefinitionFormValidator extends FormValidator {
 		if ( valid ) {
 			valid = validateInternalName(ctx, NAME_FIELD);
 			if ( valid ) {
-				//valid = validateNameUniqueness(ctx);
+				valid = validateNameUniqueness(ctx);
 			}
 		}
 		return valid;
@@ -44,8 +50,7 @@ public class NodeDefinitionFormValidator extends FormValidator {
 	protected boolean validateNameUniqueness(ValidationContext ctx) {
 		NodeDefinition editedNode = getEditedNode(ctx);
 		String name = (String) getValue(ctx, NAME_FIELD);
-		NodeDefinition sameNameDefn = getSameNameSibling(editedNode, name);
-		if ( ! sameNameDefn.getId().equals(editedNode.getId()) ) {
+		if ( existsHomonymousSibling(editedNode, name) ) {
 			String message = Labels.getLabel(NAME_ALREADY_DEFINED_MESSAGE_KEY);
 			this.addInvalidMessage(ctx, NAME_FIELD, message);
 			return false;
@@ -54,18 +59,30 @@ public class NodeDefinitionFormValidator extends FormValidator {
 		}
 	}
 
-	private NodeDefinition getSameNameSibling(NodeDefinition editedNode, String name) {
-		EntityDefinition parentDefn = (EntityDefinition) editedNode.getParentDefinition();
+	protected boolean existsHomonymousSibling(NodeDefinition defn, String name) {
+		String parentPath;
+		EntityDefinition parentDefn = (EntityDefinition) defn.getParentDefinition();
 		if ( parentDefn != null ) {
-			NodeDefinition sameNameDefn = parentDefn.getChildDefinition(name);
-			return sameNameDefn;
+			parentPath = parentDefn.getPath();
 		} else {
-			Schema schema = editedNode.getSchema();
-			EntityDefinition sameNameDefn = schema.getRootEntityDefinition(name);
-			return sameNameDefn;
+			parentPath = "";
 		}
+		Schema schema = defn.getSchema();
+		NodeDefinition nodeInPath = schema.getByPath(parentPath + "/" + name);
+		return nodeInPath != null && ! nodeInPath.getId().equals(defn.getId());
 	}
-
+	
+	protected boolean existsHomonymous(EntityDefinition editedNode, String name) {
+		Schema schema = editedNode.getSchema();
+		Collection<NodeDefinition> allDefinitions = schema.getAllDefinitions();
+		for (NodeDefinition defn : allDefinitions) {
+			if ( defn instanceof EntityDefinition && StringUtils.equals(defn.getName(), name) ) {
+				return ! defn.getId().equals(editedNode.getId());
+			}
+		}
+		return false;
+	}
+	
 	protected void validateDescription(ValidationContext ctx) {
 		//TODO
 		//Object value = getValue(ctx, DESCRIPTION_FIELD);
@@ -81,15 +98,21 @@ public class NodeDefinitionFormValidator extends FormValidator {
 	}
 	
 	protected NodeDefinition getEditedNode(ValidationContext ctx) {
-		BindContext bindContext = ctx.getBindContext();
-		Component component = bindContext.getComponent();
-		Object vmObject = component.getAttribute("viewModel");
+		Object vmObject = getVM(ctx);
 		if ( vmObject instanceof SurveySchemaEditVM ) {
 			NodeDefinition editedNode = ((SurveySchemaEditVM) vmObject).getSelectedNode();
 			return editedNode;
 		} else {
 			throw new IllegalArgumentException("Unsupported View Model Type: " + vmObject.getClass().getName());
 		}
+	}
+
+	protected Object getVM(ValidationContext ctx) {
+		BindContext bindContext = ctx.getBindContext();
+		Component component = bindContext.getComponent();
+		String vmId = (String) component.getAttribute(VM_ID_ATTRIBUTE);
+		Object vmObject = component.getAttribute(vmId);
+		return vmObject;
 	}
 
 }
