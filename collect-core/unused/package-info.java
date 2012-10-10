d87e1596e8f2bc90b5d4dcb3fcbff8f7821c9672
Stefano Ricci <stefano.ricci@fao.org>
2012-10-10 18:16:43 +0200

    Unused

---
+++ collect-core/unused/package-info.java
@@ -0,0 +1,11 @@
+@XmlSchema( namespace="http://www.openforis.org/collect/3.0/ui",
+			xmlns = {
+					@XmlNs(namespaceURI = "http://www.openforis.org/collect/3.0/ui", prefix = "ui")
+			},
+			elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED
+		)
+package org.openforis.collect.metamodel.ui;
+
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlSchema;
+  
