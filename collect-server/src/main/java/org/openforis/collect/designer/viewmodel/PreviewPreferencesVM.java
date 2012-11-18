32f137335882c9dff32d00854fc34052a4674aa8
Stefano Ricci <stefano.ricci@fao.org>
2012-11-18 13:33:39 +0100

    Code clean

--- collect-server/src/main/java/org/openforis/collect/designer/viewmodel/PreviewPreferencesVM.java
+++ collect-server/src/main/java/org/openforis/collect/designer/viewmodel/PreviewPreferencesVM.java
@@ -4,23 +4,12 @@
 package org.openforis.collect.designer.viewmodel;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.client.utils.URLEncodedUtils;
-import org.apache.http.message.BasicNameValuePair;
-import org.openforis.collect.designer.util.Resources;
 import org.openforis.collect.model.CollectSurvey;
 import org.openforis.idm.metamodel.EntityDefinition;
 import org.openforis.idm.metamodel.ModelVersion;
 import org.openforis.idm.metamodel.Schema;
-import org.zkoss.bind.BindUtils;
-import org.zkoss.bind.annotation.BindingParam;
-import org.zkoss.bind.annotation.Command;
-import org.zkoss.zk.ui.Execution;
-import org.zkoss.zk.ui.Executions;
 import org.zkoss.zkplus.databind.BindingListModelList;
 
 /**
