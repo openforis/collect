b4e249ce84915b5a54f686e3df693683fc6eace2
Stefano Ricci <stefano.ricci@fao.org>
2012-12-11 09:57:55 +0100

    Code clean

--- collect-server/src/main/java/org/openforis/collect/designer/component/UITabsTreeModel.java
+++ collect-server/src/main/java/org/openforis/collect/designer/component/UITabsTreeModel.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 import org.openforis.collect.metamodel.ui.UITab;
 import org.openforis.collect.metamodel.ui.UITabSet;
-import org.zkoss.zul.TreeNode;
 
 /**
  * @author S. Ricci
