0d1e2140ac5c8e713252d63b56c1bca327e25c11
Stefano Ricci <stefano.ricci@fao.org>
2013-01-17 11:17:20 +0100

    OFC-1111 Temp directory created in wrong location by default

--- collect-core/src/main/resources/org/openforis/collect/sql/postgresql/create-schema.sql
+++ collect-core/src/main/resources/org/openforis/collect/sql/postgresql/create-schema.sql
@@ -165,8 +165,8 @@ INSERT INTO "collect"."ofc_application_info" ("version")
 VALUES ('3.0-Alpha5');
 
 INSERT INTO "collect"."ofc_config" ("name", "value") VALUES 
-	('upload_path', '/home/openforis/collect-upload'), 
-	('index_path', '/home/openforis/collect-index');
+	('upload_path', '/home/openforis/collect-upload'); 
+	-- ('index_path', '/home/openforis/collect-index');
 
 INSERT INTO collect.ofc_user(id, username, password ,enabled)
 VALUES (nextval('collect.ofc_user_id_seq'), 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Y');
