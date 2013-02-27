e2bc4095f6804f9a1471fe94b6f6a018a86d8611
Stefano Ricci <stefano.ricci@fao.org>
2013-02-27 14:54:38 +0100

    Code clean

--- collect-server/src/main/java/org/openforis/collect/web/controller/BasicController.java
+++ collect-server/src/main/java/org/openforis/collect/web/controller/BasicController.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.openforis.collect.web.session.SessionState;
-import org.springframework.stereotype.Controller;
 
 /**
  * @author S. Ricci
