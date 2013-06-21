2b101873a4efd8211aab25de0df4e8a15fb23fa5
Stefano Ricci <stefano.ricci@fao.org>
2013-06-21 17:42:38 +0200

    OFC-1378 - Move data export service from collect-server to collect-core

---
+++ collect-server/src/main/java/org/openforis/collect/manager/dataexport/proxy/DataExportStatusProxy.java
@@ -0,0 +1,17 @@
+package org.openforis.collect.manager.dataexport.proxy;
+
+import org.openforis.collect.manager.dataexport.DataExportStatus;
+import org.openforis.collect.manager.process.proxy.ProcessStatusProxy;
+
+/**
+ * 
+ * @author S. Ricci
+ *
+ */
+public class DataExportStatusProxy extends ProcessStatusProxy {
+
+	public DataExportStatusProxy(DataExportStatus status) {
+		super(status);
+	}
+
+}
