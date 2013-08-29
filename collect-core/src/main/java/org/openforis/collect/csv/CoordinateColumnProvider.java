1bc0a32aa69da87237e34906649b6d9f85523401
Ricci Stefano <stefano.ricci@fao.org>
2013-08-29 11:23:01 +0200

    Use constant field names

--- collect-core/src/main/java/org/openforis/collect/csv/CoordinateColumnProvider.java
+++ collect-core/src/main/java/org/openforis/collect/csv/CoordinateColumnProvider.java
@@ -3,7 +3,9 @@
  */
 package org.openforis.collect.csv;
 
+import org.openforis.idm.metamodel.CoordinateAttributeDefinition;
 import org.openforis.idm.model.Attribute;
+import org.openforis.idm.model.CoordinateAttribute;
 import org.openforis.idm.model.Field;
 
 /**
@@ -14,18 +16,28 @@ import org.openforis.idm.model.Field;
 @Deprecated
 public class CoordinateColumnProvider extends CompositeAttributeColumnProvider {
 
+	private static final String FIELD_SEPARATOR = "_";
+
 	public CoordinateColumnProvider(String attributeName) {
 		super(attributeName);
 	}
 
 	@Override
 	protected String[] getFieldsHeadings() {
-		return new String[] { getAttributeName() + "_srs_id", getAttributeName() + "_x", getAttributeName() + "_y" };
+		return new String[] { 
+				getAttributeName() + FIELD_SEPARATOR + CoordinateAttributeDefinition.SRS_FIELD_NAME, 
+				getAttributeName() + FIELD_SEPARATOR + CoordinateAttributeDefinition.X_FIELD_NAME, 
+				getAttributeName() + FIELD_SEPARATOR + CoordinateAttributeDefinition.Y_FIELD_NAME };
 	}
 
 	@Override
 	protected Field<?>[] getFieldsToExtract(Attribute<?, ?> attr) {
-		return new Field[] { attr.getField(2), attr.getField(0), attr.getField(1) };
+		CoordinateAttribute coordinateAttr = (CoordinateAttribute) attr;
+		return new Field[] { 
+				coordinateAttr.getSrsIdField(), 
+				coordinateAttr.getXField(), 
+				coordinateAttr.getYField()
+		};
 	}
 
 }
