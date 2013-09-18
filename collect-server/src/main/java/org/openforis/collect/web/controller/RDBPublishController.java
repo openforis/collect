7f6cdafabb2c88be2253384535dfbef620271a1e
Ricci Stefano <stefano.ricci@fao.org>
2013-09-18 10:13:01 +0200

    Change RDBPublishController access url and paramters

--- collect-server/src/main/java/org/openforis/collect/web/controller/RDBPublishController.java
+++ collect-server/src/main/java/org/openforis/collect/web/controller/RDBPublishController.java
@@ -1,8 +1,13 @@
 package org.openforis.collect.web.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import org.openforis.collect.manager.CollectRDBPublisher;
 import org.openforis.collect.model.CollectRecord.Step;
 import org.openforis.collect.relational.CollectRdbException;
+import org.openforis.collect.relational.model.RelationalSchemaConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,25 +26,27 @@ public class RDBPublishController {
 	@Autowired
 	private CollectRDBPublisher rdbPublisher;
 	
-	@RequestMapping(value = "/submitPublishToRdb", method = RequestMethod.POST)
+	@RequestMapping(value = "/rdbpublish", method = RequestMethod.GET)
 	public @ResponseBody String publish(
 			@RequestParam("survey") String surveyName, 
 			@RequestParam("root_entity") String rootEntityName, 
-			@RequestParam("schema") String schemaName/*,
-			@RequestParam("url") String targetUrl,
-			@RequestParam("user") String targetUser,
-			@RequestParam("pass") String targetPassword*/) throws CollectRdbException {
+			@RequestParam("targeturl") String targetUrl,
+			@RequestParam("targetuser") String targetUser,
+			@RequestParam("targetpass") String targetPassword,
+			@RequestParam("targetschema") String schemaName
+			) throws CollectRdbException, SQLException {
 		
-		//Connection targetConn = DriverManager.getConnection(targetUrl, targetUser, targetPassword);
-		//DriverManager.getConnection("jdbc:postgresql://localhost:5433/archenland1", "postgres","postgres"),
+		Connection targetConn = DriverManager.getConnection(targetUrl, targetUser, targetPassword);
+//		DriverManager.getConnection("jdbc:postgresql://localhost:5433/archenland1", "postgres","postgres"),
 	
 		rdbPublisher.export(
 					surveyName,
 					rootEntityName,
 					Step.ANALYSIS,
-					schemaName/*,
-					targetConn*/);
-		return "Publishing, please check log files for progress updates.";
+					schemaName,
+					targetConn,
+					RelationalSchemaConfig.createDefault());
+		return "Publishing, please check log files in order to monitor the process.";
 	}
 	
 }
