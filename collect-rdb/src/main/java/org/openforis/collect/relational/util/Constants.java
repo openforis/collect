3af5d42e324e3a875656d9e9b69b6a3e002712f4
Ricci Stefano <stefano.ricci@fao.org>
2014-02-14 18:44:49 +0100

    Added utility class for code list table naming

---
+++ collect-rdb/src/main/java/org/openforis/collect/relational/util/Constants.java
@@ -0,0 +1,18 @@
+package org.openforis.collect.relational.util;
+
+import javax.xml.namespace.QName;
+
+/**
+ * 
+ * @author S. Ricci
+ *
+ */
+public interface Constants {
+	
+	public static final String RDB_NAMESPACE = "http://www.openforis.org/collect/3.0/rdb";
+	public static final QName TABLE_NAME_QNAME = new QName(RDB_NAMESPACE, "table");
+	public static final QName COLUMN_NAME_QNAME = new QName(RDB_NAMESPACE, "column");
+	public static final String DATA_TABLE_PK_FORMAT = "_%s%s";
+	public static final String CODE_TABLE_PK_FORMAT = "%s%s";
+	
+}
