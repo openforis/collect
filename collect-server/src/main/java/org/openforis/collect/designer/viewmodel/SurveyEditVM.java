c6fa3a3fb60a1631b9807406b9d8fcb903d326ae
Stefano Ricci <stefano.ricci@fao.org>
2014-11-05 18:41:31 +0100

    Avoid editing sampling design code list

--- collect-server/src/main/java/org/openforis/collect/designer/viewmodel/SurveyEditVM.java
+++ collect-server/src/main/java/org/openforis/collect/designer/viewmodel/SurveyEditVM.java
@@ -142,11 +142,15 @@ public class SurveyEditVM extends SurveyBaseVM {
 			@BindingParam(CodeListsVM.EDITING_ATTRIBUTE_PARAM) Boolean editingAttribute, 
 			@BindingParam(CodeListsVM.SELECTED_CODE_LIST_PARAM) CodeList selectedCodeList) {
 		if ( codeListsPopUp == null ) { 
-			dispatchCurrentFormValidatedCommand(true);
-			Map<String, Object> args = new HashMap<String, Object>();
-			args.put(CodeListsVM.EDITING_ATTRIBUTE_PARAM, editingAttribute);
-			args.put(CodeListsVM.SELECTED_CODE_LIST_PARAM, selectedCodeList);
-			codeListsPopUp = openPopUp(Resources.Component.CODE_LISTS_POPUP.getLocation(), true, args);
+			if (selectedCodeList == survey.getSamplingDesignCodeList()) {
+				MessageUtil.showWarning("survey.code_list.alert.cannot_edit_sampling_design_list");
+			} else {
+				dispatchCurrentFormValidatedCommand(true);
+				Map<String, Object> args = new HashMap<String, Object>();
+				args.put(CodeListsVM.EDITING_ATTRIBUTE_PARAM, editingAttribute);
+				args.put(CodeListsVM.SELECTED_CODE_LIST_PARAM, selectedCodeList);
+				codeListsPopUp = openPopUp(Resources.Component.CODE_LISTS_POPUP.getLocation(), true, args);
+			}
 		}
 	}
 
