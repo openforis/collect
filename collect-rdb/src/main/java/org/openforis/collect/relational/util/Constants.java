5446ad5f59497ea380dc58666c3bf6e1cfab8dbf
Stefano Ricci <stefano.ricci@fao.org>
2015-03-11 17:40:13 +0100

    Use configurable id column prefix in RDB

--- collect-rdb/src/main/java/org/openforis/collect/relational/util/Constants.java
+++ collect-rdb/src/main/java/org/openforis/collect/relational/util/Constants.java
@@ -12,7 +12,7 @@ public interface Constants {
 	public static final String RDB_NAMESPACE = "http://www.openforis.org/collect/3.0/rdb";
 	public static final QName TABLE_NAME_QNAME = new QName(RDB_NAMESPACE, "table");
 	public static final QName COLUMN_NAME_QNAME = new QName(RDB_NAMESPACE, "column");
-	public static final String DATA_TABLE_PK_FORMAT = "%s%s";
+	public static final String DATA_TABLE_PK_FORMAT = "%s%s%s";
 	public static final String CODE_TABLE_PK_FORMAT = "%s%s";
 	
 }
