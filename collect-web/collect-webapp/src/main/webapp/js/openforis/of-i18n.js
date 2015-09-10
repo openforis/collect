00b14e031d5f2cdabf1de936f21d66053c6031f7
Stefano Ricci <stefano.ricci@fao.org>
2015-09-10 14:57:55 +0200

    Code clean

--- collect-web/collect-webapp/src/main/webapp/js/openforis/of-i18n.js
+++ collect-web/collect-webapp/src/main/webapp/js/openforis/of-i18n.js
@@ -1,16 +1,18 @@
 OF.i18n = {};
 
+OF.i18n.MESSAGE_KEY_DATA_PROPERTY_NAME = "i18n";
+
 OF.i18n.initAll = function(el) {
 	if (! el) {
 		el = document;
 	}
-	$(el).find("[data-messagekey]").each(function(index, subEl) {
+	$(el).find("[data-" + OF.i18n.MESSAGE_KEY_DATA_PROPERTY_NAME + "]").each(function(index, subEl) {
 		OF.i18n.initEl(subEl);
 	});
 };
 
 OF.i18n.initEl = function(el) {
-	var messageKey = $(el).data("messagekey");
+	var messageKey = $(el).data(OF.i18n.MESSAGE_KEY_DATA_PROPERTY_NAME);
 	if (messageKey) {
 		var message = jQuery.i18n.prop(messageKey);
 		$(el).html(message);
