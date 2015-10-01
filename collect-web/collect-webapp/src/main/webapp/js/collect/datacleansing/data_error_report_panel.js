225051a4a6dd354082f865828a7421eae2c838ce
Stefano Ricci <stefano.ricci@fao.org>
2015-10-01 15:40:14 +0200

    Include affected records percent in data error report view

--- collect-web/collect-webapp/src/main/webapp/js/collect/datacleansing/data_error_report_panel.js
+++ collect-web/collect-webapp/src/main/webapp/js/collect/datacleansing/data_error_report_panel.js
@@ -20,6 +20,7 @@ Collect.DataCleansing.DataErrorReportPanelController.prototype.getDataGridOption
 			{field: "lastRecordModifiedDate", title: "Last Record Modified", formatter: OF.Dates.formatToPrettyDateTime, sortable: true, width: 130},
 			{field: "itemCount", title: "Errors found", sortable: true, width: 100},
 			{field: "affectedRecordsCount", title: "Affected Records", sortable: false, width: 120},
+			{field: "affectedRecordsPercent", title: "Affected Records %", sortable: true, width: 100},
 			{field: "creationDate", title: "Creation Date", formatter: OF.Dates.formatToPrettyDateTime, sortable: true, width: 130},
 			$this.createGridItemDeleteColumn()
 		]
