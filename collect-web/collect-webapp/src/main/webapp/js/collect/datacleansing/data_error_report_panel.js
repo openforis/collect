903c06ba4743fc1f0fb38570e32e53420f1ff495
Stefano Ricci <stefano.ricci@fao.org>
2015-10-01 15:49:31 +0200

    Show affected records percent in data error report list

--- collect-web/collect-webapp/src/main/webapp/js/collect/datacleansing/data_error_report_panel.js
+++ collect-web/collect-webapp/src/main/webapp/js/collect/datacleansing/data_error_report_panel.js
@@ -9,6 +9,11 @@ Collect.DataCleansing.DataErrorReportPanelController.prototype = Object.create(C
 
 Collect.DataCleansing.DataErrorReportPanelController.prototype.getDataGridOptions = function() {
 	var $this = this;
+	
+	function percentFormatter(value, row) {
+		return value.toFixed(1) + " %";
+    }
+	
 	return {
 	    url: "datacleansing/dataerrorreports/list.json",
 	    height: 400,
@@ -16,12 +21,12 @@ Collect.DataCleansing.DataErrorReportPanelController.prototype.getDataGridOption
           	{field: "selected", title: "", radio: true},
 			{field: "id", title: "Id", visible: false},
 			{field: "queryGroupTitle", title: "Query Group", sortable: true, width: 300},
-			{field: "datasetSize", title: "Dataset Size", sortable: true, width: 100},
-			{field: "lastRecordModifiedDate", title: "Last Record Modified", formatter: OF.Dates.formatToPrettyDateTime, sortable: true, width: 130},
-			{field: "itemCount", title: "Errors found", sortable: true, width: 100},
-			{field: "affectedRecordsCount", title: "Affected Records", sortable: false, width: 120},
-			{field: "affectedRecordsPercent", title: "Affected Records %", sortable: true, width: 100},
-			{field: "creationDate", title: "Creation Date", formatter: OF.Dates.formatToPrettyDateTime, sortable: true, width: 130},
+			{field: "datasetSize", title: "Dataset Size", align: "right", sortable: true, width: 100},
+			{field: "lastRecordModifiedDate", title: "Last Record Modified", formatter: OF.Dates.formatToPrettyDateTime, align: "right", sortable: true, width: 130},
+			{field: "itemCount", title: "Errors found", align: "right", sortable: true, width: 100},
+			{field: "affectedRecordsCount", title: "Affected Records", align: "right", sortable: false, width: 120},
+			{field: "affectedRecordsPercent", title: "Affected Records %", formatter: percentFormatter, align: "right", sortable: true, width: 100},
+			{field: "creationDate", title: "Creation Date", formatter: OF.Dates.formatToPrettyDateTime, align: "right", sortable: true, width: 130},
 			$this.createGridItemDeleteColumn()
 		]
 	};
