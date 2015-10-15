2646df5913a1c8ede191b451ff20bc55ea62455e
Stefano Ricci <stefano.ricci@fao.org>
2015-10-15 11:11:35 +0200

    Code clean, add helper function

--- collect-web/collect-webapp/src/main/webapp/js/openforis/of-strings.js
+++ collect-web/collect-webapp/src/main/webapp/js/openforis/of-strings.js
@@ -36,3 +36,10 @@ OF.Strings.rightPad = function(str, pad, length) {
 	}
 	return str;
 }
+
+OF.Strings.format = function(format) {
+    var args = Array.prototype.slice.call(arguments, 1);
+	return format.replace(/{(\d+)}/g, function(match, number) {
+		return typeof args[number] != 'undefined' ? args[number] : match;
+	});
+};
