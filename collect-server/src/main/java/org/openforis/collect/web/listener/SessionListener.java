1a2c2ee026756a42e118d5ba18f2033cbea6d710
fefrain <fefrain@users.noreply.github.com>
2015-10-20 15:53:28 +0200

    Remove Saiku integration

--- collect-server/src/main/java/org/openforis/collect/web/listener/SessionListener.java
+++ collect-server/src/main/java/org/openforis/collect/web/listener/SessionListener.java
@@ -11,7 +11,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openforis.collect.manager.SessionManager;
 import org.openforis.collect.model.User;
-import org.openforis.collect.saiku.SaikuSessionService;
 import org.openforis.collect.web.session.InvalidSessionException;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
@@ -30,9 +29,6 @@ public class SessionListener implements HttpSessionListener {
 		SessionManager sessionManager = getSessionManager(se);
 		sessionManager.createSessionState(se.getSession());
 		
-//		SaikuSessionService saikuSessionService = getSaikuSessionService(se);
-//		saikuSessionService.createSession();
-		
 		if ( LOG.isInfoEnabled() ) {
 			LOG.info("Session created: " + se.getSession().getId());
 		}
@@ -45,8 +41,6 @@ public class SessionListener implements HttpSessionListener {
 			User user = sessionManager.getSessionState().getUser();
 			if (user != null) {
 				sessionManager.sessionDestroyed();
-				SaikuSessionService saikuSessionService = getSaikuSessionService(se);
-				saikuSessionService.clearSession(user.getName());
 			}
 			if ( LOG.isInfoEnabled() ) {
 				String message = "Session destroyed: " + se.getSession().getId();
@@ -73,21 +67,10 @@ public class SessionListener implements HttpSessionListener {
 //		}
 	}
 
-	private SaikuSessionService getSaikuSessionService(HttpSessionEvent se) {
-		return getBean(se, SaikuSessionService.class);
-	}
-
 	private SessionManager getSessionManager(HttpSessionEvent se) {
 		return getBean(se, "sessionManager");
 	}
 
-	private <T extends Object> T getBean(HttpSessionEvent se,
-			Class<T> type) {
-		ServletContext sc = se.getSession().getServletContext();
-		WebApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(sc);
-		return applicationContext.getBean(type);
-	}
-	
 	@SuppressWarnings("unchecked")
 	private <T extends Object> T getBean(HttpSessionEvent se,
 			String name) {
