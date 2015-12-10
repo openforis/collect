00c302c679d275c023090965f54ca90efa8d9b20
herrtunante <herrtunante@users.noreply.github.com>
2015-12-10 15:17:19 +0100

    Fix headers for ancillary data

--- collect-server/src/main/java/org/openforis/collect/io/metadata/collectearth/balloon/CEComponentHTMLFormatter.java
+++ collect-server/src/main/java/org/openforis/collect/io/metadata/collectearth/balloon/CEComponentHTMLFormatter.java
@@ -119,7 +119,7 @@ public class CEComponentHTMLFormatter {
 		for (CEComponent child : comp.getChildren()) {
 			if (child instanceof CEField) {
 
-				informationFieldsBuilder.e("span" ).t( (firstChild?" ":", ") + child.getLabelOrName() + ": $[" + child.getName()+ "]" );
+				informationFieldsBuilder.e("span" ).t( (firstChild?" ":", ") + child.getLabelOrName() + ": $["+  CollectEarthBalloonGenerator.PREFIX_EXTRA_CSV_FIELD + child.getName()+ "]" );
 				firstChild = false;
 				
 			} else {
