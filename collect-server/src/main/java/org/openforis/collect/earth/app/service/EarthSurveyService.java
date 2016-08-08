7a6ead9cc20b4459fd9af5399cea4ffaa55cdaf1
fefrain <fefrain@users.noreply.github.com>
2016-08-08 17:56:43 +0200

    Use latest CE version

---
+++ collect-server/src/main/java/org/openforis/collect/earth/app/service/EarthSurveyService.java
@@ -0,0 +1,8 @@
+package org.openforis.collect.earth.app.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EarthSurveyService extends AbstractEarthSurveyService {
+
+}
