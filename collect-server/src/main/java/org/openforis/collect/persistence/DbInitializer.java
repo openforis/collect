a27d67ea209619c93c7ce2e684cfd284435314da
fefrain <fefrain@users.noreply.github.com>
2017-03-27 14:13:09 +0200

    Improve db initialization

--- collect-server/src/main/java/org/openforis/collect/persistence/DbInitializer.java
+++ collect-server/src/main/java/org/openforis/collect/persistence/DbInitializer.java
@@ -1,7 +1,9 @@
 package org.openforis.collect.persistence;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Locale;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -39,11 +41,17 @@ public class DbInitializer {
 			LOG.info(String.format("Creating schema %s if not exists...", DbUtils.SCHEMA_NAME));
 
 			if (createSchemaIfNotExists(conn)) {
-				createSchema(conn);
+				LOG.info(String.format("Schema '%s' created (if not existing already)", DbUtils.SCHEMA_NAME));
+			} else {
+				LOG.info("Try to create schema withoud 'NOT EXISTS' clause");
+				boolean schemaCreated = createSchema(conn);
+				if (schemaCreated) {
+					LOG.info(String.format("Schema '%s' created", DbUtils.SCHEMA_NAME));
+				} else {
+					LOG.info(String.format("Schema '%s' already exists", DbUtils.SCHEMA_NAME));
+				}
 			}
-			LOG.info(String.format("Schema '%s' created (if not existing already)", DbUtils.SCHEMA_NAME));
 		} catch (Exception e) {
-			
 		} finally {
 			if (conn != null) {
 				connectionProvider.release(conn);
@@ -51,27 +59,54 @@ public class DbInitializer {
 		}
 	}
 
-	private void createSchema(Connection conn) {
-		try {
-			Statement stmt = conn.createStatement();
-			stmt.execute(String.format("CREATE SCHEMA '%s'", DbUtils.SCHEMA_NAME));
-			conn.commit();
-		} catch(Exception e) {
-			String errorMessage = String.format("Error creating schema '%s'", DbUtils.SCHEMA_NAME);
-			LOG.error(errorMessage, e);
-			throw new RuntimeException(errorMessage, e);
-		}
-	}
-
+	/**
+	 * Creates the schema using a CREATE SCHEMA IF NOT EXISTS statement.
+	 * 
+	 * @return 'true' if the statement is executed correctly, false if the syntax is not supported
+	 */
 	private boolean createSchemaIfNotExists(Connection conn) {
 		try {
 			Statement stmt = conn.createStatement();
-			stmt.execute(String.format("CREATE SCHEMA IF NOT EXISTS '%s'", DbUtils.SCHEMA_NAME));
-			conn.commit();
+			stmt.execute(String.format("CREATE SCHEMA IF NOT EXISTS %s", DbUtils.SCHEMA_NAME));
 			return true;
 		} catch(Exception e) {
-			//create schema if not exists not supported
+			LOG.info("CREATE SCHEMA IF NOT EXISTS is not supported by this DBMS");
 			return false;
+		} finally {
+			//commit transaction anyway
+			commitQuietly(conn);
 		}
 	}
+	
+	/**
+	 * Creates a new schema using a CREATE SCHEMA statement.
+	 * 
+	 * @return 'true' if the schema is created, false if it already exists
+	 */
+	private boolean createSchema(Connection conn) {
+		try {
+			Statement stmt = conn.createStatement();
+			stmt.execute(String.format("CREATE SCHEMA %s", DbUtils.SCHEMA_NAME));
+			return true;
+		} catch(Exception e) {
+			boolean schemaAlreadyExists = e.getMessage().toLowerCase(Locale.ENGLISH).contains("already exists");
+			if (schemaAlreadyExists) {
+				return false;
+			} else {
+				String errorMessage = String.format("Error creating schema '%s'", DbUtils.SCHEMA_NAME);
+				LOG.error(errorMessage, e);
+				throw new RuntimeException(errorMessage, e);
+			}
+		} finally {
+			//commit transaction anyway
+			commitQuietly(conn);
+		}
+	}
+
+	private void commitQuietly(Connection conn) {
+		try {
+			conn.commit();
+		} catch (SQLException e) {}
+	}
+	
 }
