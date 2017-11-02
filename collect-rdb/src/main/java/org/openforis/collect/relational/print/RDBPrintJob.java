91caa6e117f1b56fa02c2e9d34503715fdcaafd7
fefrain <fefrain@users.noreply.github.com>
2017-11-02 12:24:48 +0100

    Always include code list values in SQL exporter

--- collect-rdb/src/main/java/org/openforis/collect/relational/print/RDBPrintJob.java
+++ collect-rdb/src/main/java/org/openforis/collect/relational/print/RDBPrintJob.java
@@ -198,6 +198,7 @@ public class RDBPrintJob extends Job {
 					return;
 				}
 				CodeTableDataExtractor extractor = DataExtractorFactory.getExtractor(codeTable);
+				writeBatchInsert(codeTable, extractor);
 				setProcessedItems(getProcessedItems() + extractor.getTotal());
 			}
 		}
@@ -218,12 +219,12 @@ public class RDBPrintJob extends Job {
 		protected void execute() throws Throwable {
 			recordManager.visitSummaries(recordFilter, null, new Visitor<CollectRecord>() {
 				public void visit(CollectRecord summary) {
+					if(!isRunning()) {
+						return;
+					}
 					CollectRecord record = recordManager.load((CollectSurvey) summary.getSurvey(), summary.getId(), summary.getStep());
 					if (record != null) {
 						for (DataTable table : schema.getDataTables()) {
-							if(!isRunning()) {
-								return;
-							}
 							try {
 								writeBatchInsert(table, DataExtractorFactory.getRecordDataExtractor(table, record));
 							} catch(Exception e) {
