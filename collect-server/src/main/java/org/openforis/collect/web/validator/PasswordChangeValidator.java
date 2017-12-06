162f08881f2fc0e464c26d535d56007220646d91
fefrain <fefrain@users.noreply.github.com>
2017-12-06 15:57:35 +0100

    Password change function

---
+++ collect-server/src/main/java/org/openforis/collect/web/validator/PasswordChangeValidator.java
@@ -0,0 +1,63 @@
+package org.openforis.collect.web.validator;
+
+import static org.springframework.web.context.WebApplicationContext.SCOPE_SESSION;
+
+import org.apache.commons.lang3.StringUtils;
+import org.openforis.collect.datacleansing.form.validation.SimpleValidator;
+import org.openforis.collect.manager.SessionManager;
+import org.openforis.collect.manager.UserManager;
+import org.openforis.collect.model.User;
+import org.openforis.collect.web.controller.UserController.PasswordChangeParameters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+@Component
+@Scope(SCOPE_SESSION)
+public class PasswordChangeValidator extends SimpleValidator<PasswordChangeParameters> {
+
+	private static final String WRONG_PASSWORD_SPECIFIED_MESSAGE_KEY = "user.validation.wrongPasswordSpecified";
+	private static final String OLD_PASSWORD_FIELD = "oldPassword";
+	private static final String NEW_PASSWORD_FIELD = "newPassword";
+	private static final String RETYPED_PASSWORD_FIELD = "retypedPassword";
+	
+	@Autowired
+	private UserManager userManager;
+	@Autowired
+	private SessionManager sessionManager;
+	
+	@Override
+	public void validateForm(PasswordChangeParameters target, Errors errors) {
+		validateOldPassword(errors);
+		validateNewPassword(errors);
+		validateRetypedPassword(errors);
+	}
+
+	private void validateOldPassword(Errors errors) {
+		if (validateRequiredField(errors, OLD_PASSWORD_FIELD)) {
+			String oldPassword = (String) errors.getFieldValue(OLD_PASSWORD_FIELD);
+			User loggedUser = sessionManager.getLoggedUser();
+			if (! userManager.verifyPassword(loggedUser.getUsername(), oldPassword)) {
+				errors.rejectValue(OLD_PASSWORD_FIELD, WRONG_PASSWORD_SPECIFIED_MESSAGE_KEY);
+			}
+		}
+	}
+	
+	private void validateNewPassword(Errors errors) {
+		if (validateRequiredField(errors, NEW_PASSWORD_FIELD)) {
+			String newPassword = (String) errors.getFieldValue(NEW_PASSWORD_FIELD);
+			UserValidator.validatePassword(errors, newPassword, NEW_PASSWORD_FIELD);
+		}
+	}
+
+	private void validateRetypedPassword(Errors errors) {
+		if (validateRequiredField(errors, RETYPED_PASSWORD_FIELD)) {
+			String newPassword = (String) errors.getFieldValue(NEW_PASSWORD_FIELD);
+			String retypedPassword = (String) errors.getFieldValue(RETYPED_PASSWORD_FIELD);
+			if (! StringUtils.equals(newPassword, retypedPassword)) {
+				errors.rejectValue(RETYPED_PASSWORD_FIELD, UserValidator.WRONG_RETYPED_PASSWORD_MESSAGE_KEY);
+			}
+		}
+	}
+}
---
+++ collect-web/collect-webapp/frontend/src/containers/users/PasswordChangePage.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { Field, SubmissionError, reduxForm } from 'redux-form'
+import Button from 'material-ui/Button';
+
+import ServiceFactory from 'services/ServiceFactory';
+import Forms from 'components/Forms';
+import L from 'utils/Labels';
+import RouterUtils from 'utils/RouterUtils';
+
+const asyncValidate = (values /*, dispatch */) => {
+    return ServiceFactory.userService.validatePasswordChange(values.oldPassword, values.newPassword, values.retypedPassword)
+        .then(r => Forms.handleValidationResponse(r))
+}
+
+class PasswordChangePage extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.submit = this.submit.bind(this)
+    }
+
+    submit(values) {
+        return ServiceFactory.userService.changePassword(values.oldPassword, values.newPassword)
+            .then(r => {
+                Forms.handleValidationResponse(r)
+                if (r.statusOk) {
+                    alert('Password changed!') //TODO improve alert dialogs!!!
+                    RouterUtils.navigateToHomePage(this.props.history)
+                }
+            })
+    }
+    
+    render() {
+        const { handleSubmit, pristine, reset, submitting, anyTouched, error } = this.props
+
+        return (
+            <form onSubmit={handleSubmit(this.submit)} style={{width: '700px'}}>
+                <Field
+                    name="oldPassword"
+                    type="password"
+                    component={Forms.renderFormItemInputField}
+                    fullWidth={true}
+                    labelColSpan={3}
+                    fieldColSpan={9}
+                    label={L.l('user.oldPassword')}
+                    />
+                <Field
+                    name="newPassword"
+                    type="password"
+                    component={Forms.renderFormItemInputField}
+                    fullWidth={true}
+                    labelColSpan={3}
+                    fieldColSpan={9}
+                    label={L.l('user.newPassword')}
+                    />
+                <Field
+                    name="retypedPassword"
+                    type="password"
+                    component={Forms.renderFormItemInputField}
+                    fullWidth={true}
+                    labelColSpan={3}
+                    fieldColSpan={9}
+                    label={L.l('user.retypedPassword')}
+                    />
+                {error && <div class="error">{error}</div>}
+                <div>
+                    <Button raised color="primary" disabled={!anyTouched || error || submitting} type="submit">
+                        {L.l('user.changePassword.change')}
+                    </Button>
+                </div>
+            </form>
+        )
+    }
+}
+
+export default reduxForm({ 
+    form: 'passwordChangeForm',
+    initialValues: {
+        oldPassword: '',
+        newPassword: '',
+        retypedPassword: ''
+    },
+    asyncValidate,
+    asyncBlurFields: ['oldPassword', 'newPassword', 'retypedPassword']
+})(PasswordChangePage)
