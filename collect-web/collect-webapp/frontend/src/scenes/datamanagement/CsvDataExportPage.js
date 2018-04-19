37a8e26943f80b030ee61de6fabcc5b43e3f1e8d
fefrain <fefrain@users.noreply.github.com>
2018-04-19 10:12:37 +0200

    Fixed schema tree view style

--- collect-web/collect-webapp/frontend/src/scenes/datamanagement/CsvDataExportPage.js
+++ collect-web/collect-webapp/frontend/src/scenes/datamanagement/CsvDataExportPage.js
@@ -137,77 +137,75 @@ class CsvDataExportPage extends Component {
                     <Form>
                         <FormGroup tag="fieldset">
                             <legend>Parameters</legend>
-                            <Form>
-                                <FormGroup row>
-                                    <Label for="stepSelect" sm={1}>Step:</Label>
-                                    <Col sm={10}>
-                                        <Input type="select" name="step" id="stepSelect" style={{ maxWidth: '100px' }} 
-                                            value={this.state.stepGreaterOrEqual}
-                                            onChange={e => this.setState({stepGreaterOrEqual: e.target.value})}>{stepsOptions}</Input>
-                                    </Col>
-                                </FormGroup>
-                                <FormGroup row>
-                                    <Label for="exportMode" sm={1}>Export mode:</Label>
-                                    <Col sm={10}>
-                                        <FormGroup check>
-                                            <Label check>
-                                                <Input type="radio" value="ALL_ENTITIES" name="exportMode"
-                                                    checked={this.state.exportMode === 'ALL_ENTITIES'} 
-                                                    onChange={(event) => this.setState({...this.state, exportMode: event.target.value})} />
-                                                All entities
-                                            </Label>
-                                            <span style={{display: 'inline-block', width: '40px'}}></span>
-                                            <Label check>
-                                                <Input type="radio" value="SELECTED_ENTITY" name="exportMode"
-                                                    checked={this.state.exportMode === 'SELECTED_ENTITY'} 
-                                                    onChange={(event) => this.setState({...this.state, exportMode: event.target.value})} />
-                                                Only selected entities
-                                            </Label>
-                                        </FormGroup>
-                                    </Col>
-                                </FormGroup>
-                                {this.state.exportMode === 'SELECTED_ENTITY' &&
-                                    <FormGroup row>
-                                        <Label sm={1}>Select entities to export:</Label>
-                                        <Col sm={{size: 10 }}>
-                                            <SchemaTreeView survey={this.props.survey}
-                                                handleNodeSelect={this.handleEntitySelect} />
-                                        </Col>
+                            <FormGroup row>
+                                <Label for="stepSelect" sm={1}>Step:</Label>
+                                <Col sm={10}>
+                                    <Input type="select" name="step" id="stepSelect" style={{ maxWidth: '100px' }} 
+                                        value={this.state.stepGreaterOrEqual}
+                                        onChange={e => this.setState({stepGreaterOrEqual: e.target.value})}>{stepsOptions}</Input>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label for="exportMode" sm={1}>Export mode:</Label>
+                                <Col sm={10}>
+                                    <FormGroup check>
+                                        <Label check>
+                                            <Input type="radio" value="ALL_ENTITIES" name="exportMode"
+                                                checked={this.state.exportMode === 'ALL_ENTITIES'} 
+                                                onChange={(event) => this.setState({...this.state, exportMode: event.target.value})} />
+                                            All entities
+                                        </Label>
+                                        <span style={{display: 'inline-block', width: '40px'}}></span>
+                                        <Label check>
+                                            <Input type="radio" value="SELECTED_ENTITY" name="exportMode"
+                                                checked={this.state.exportMode === 'SELECTED_ENTITY'} 
+                                                onChange={(event) => this.setState({...this.state, exportMode: event.target.value})} />
+                                            Only selected entities
+                                        </Label>
                                     </FormGroup>
-                                }
+                                </Col>
+                            </FormGroup>
+                            {this.state.exportMode === 'SELECTED_ENTITY' &&
                                 <FormGroup row>
-                                    <ExpansionPanel>
-                                        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                                            <Typography>{L.l('general.additionalOptions')}</Typography>
-                                        </ExpansionPanelSummary>
-                                        <ExpansionPanelDetails>
-                                            <div>
-                                                <FormGroup check row>
-                                                    <Label check>
-                                                        <Input type="checkbox" onChange={event => this.setState({exportOnlyOwnedRecords: event.target.checked})} 
-                                                            checked={this.state.exportOnlyOwnedRecords} />{' '}
-                                                        Export only owned records
-                                                    </Label>
-                                                </FormGroup>
-                                                <FormGroup row>
-                                                    <Col sm={6}>
-                                                        <Label for="headingsSourceSelect">Source for file headings:</Label>
-                                                    </Col>
-                                                    <Col sm={6}>
-                                                        <Input type="select" name="headingsSource" id="headingsSourceSelect" style={{ maxWidth: '200px' }} 
-                                                            onChange={e => this.setState({headingSource: e.target.value})}>
-                                                            <option value="ATTRIBUTE_NAME">Attribute name</option>
-                                                            <option value="INSTANCE_LABEL">Attribute label</option>
-                                                            <option value="REPORTING_LABEL">Reporting label (Saiku)</option>
-                                                        </Input>
-                                                    </Col>
-                                                </FormGroup>
-                                                {additionalOptionsFormGroups}
-                                            </div>
-                                        </ExpansionPanelDetails>
-                                    </ExpansionPanel>
+                                    <Label sm={1}>Select entities to export:</Label>
+                                    <Col sm={{size: 10 }}>
+                                        <SchemaTreeView survey={this.props.survey}
+                                            handleNodeSelect={this.handleEntitySelect} />
+                                    </Col>
                                 </FormGroup>
-                            </Form>
+                            }
+                            <FormGroup row>
+                                <ExpansionPanel>
+                                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                                        <Typography>{L.l('general.additionalOptions')}</Typography>
+                                    </ExpansionPanelSummary>
+                                    <ExpansionPanelDetails>
+                                        <div>
+                                            <FormGroup check row>
+                                                <Label check>
+                                                    <Input type="checkbox" onChange={event => this.setState({exportOnlyOwnedRecords: event.target.checked})} 
+                                                        checked={this.state.exportOnlyOwnedRecords} />{' '}
+                                                    Export only owned records
+                                                </Label>
+                                            </FormGroup>
+                                            <FormGroup row>
+                                                <Col sm={6}>
+                                                    <Label for="headingsSourceSelect">Source for file headings:</Label>
+                                                </Col>
+                                                <Col sm={6}>
+                                                    <Input type="select" name="headingsSource" id="headingsSourceSelect" style={{ maxWidth: '200px' }} 
+                                                        onChange={e => this.setState({headingSource: e.target.value})}>
+                                                        <option value="ATTRIBUTE_NAME">Attribute name</option>
+                                                        <option value="INSTANCE_LABEL">Attribute label</option>
+                                                        <option value="REPORTING_LABEL">Reporting label (Saiku)</option>
+                                                    </Input>
+                                                </Col>
+                                            </FormGroup>
+                                            {additionalOptionsFormGroups}
+                                        </div>
+                                    </ExpansionPanelDetails>
+                                </ExpansionPanel>
+                            </FormGroup>
                         </FormGroup>
                         <Row>
                             <Col sm={{ size: 'auto', offset: 5 }}>
