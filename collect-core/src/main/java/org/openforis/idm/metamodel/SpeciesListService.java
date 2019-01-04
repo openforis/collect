f62819621b1c059ca9ee5111e39c112dfe08ec59
fefrain <fefrain@users.noreply.github.com>
2019-01-04 09:04:56 +0100

    Fill all fields when applying taxon default value

--- collect-core/src/main/java/org/openforis/idm/metamodel/SpeciesListService.java
+++ collect-core/src/main/java/org/openforis/idm/metamodel/SpeciesListService.java
@@ -2,10 +2,14 @@ package org.openforis.idm.metamodel;
 
 import java.util.List;
 
+import org.openforis.idm.model.species.Taxon;
+
 public interface SpeciesListService {
 
 	List<String> loadSpeciesListNames(Survey survey);
 	
 	Object loadSpeciesListData(Survey survey, String taxonomy, String attribute, String speciesCode);
+	
+	Taxon loadTaxonByCode(Survey survey, String taxonomy, String code);
 	
 }
