3dffbd6128374422f45318d67c993401e77369cc
Stefano Ricci <SteRiccio@users.noreply.github.com>
2020-03-25 03:22:28 +0100

    Fixed: file dropzone not highlighted on file drop

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [3.26.6] - 2020-03-25
+### Fixed
+- File dropzone not highlighted on file drop
+
 ## [3.26.5] - 2020-03-24
 ### Fixed
 - Error "You are editing another record" in survey preview
--- collect-webapp/frontend/src/common/components/Dropzone.js
+++ collect-webapp/frontend/src/common/components/Dropzone.js
@@ -1,49 +1,47 @@
 import './dropzone.css'
 
-import React from 'react';
-import ReactDropzone from 'react-dropzone';
+import React from 'react'
+import { useDropzone } from 'react-dropzone'
 
-import L from 'utils/Labels';
+import L from 'utils/Labels'
 
-export default class Dropzone extends React.Component {
+const Dropzone = props => {
 
-    constructor(props) {
-        super(props)
+    const { handleFileDrop, acceptedFileTypes, acceptedFileTypesDescription, fileToBeImportedPreview, width = '100%', height = '200px' } = props
 
-        this.handleFilesDrop = this.handleFilesDrop.bind(this)
-    }
-
-    handleFilesDrop(acceptedFiles) {
+    const handleFilesDrop = acceptedFiles => {
         //only single file upload supported
         if (acceptedFiles !== null && acceptedFiles.length === 1) {
             const file = acceptedFiles[0]
-            this.props.handleFileDrop(file)
+            handleFileDrop(file)
         }
     }
 
-    render() {
-        const { acceptedFileTypes, acceptedFileTypesDescription, fileToBeImportedPreview, width = '100%', height = '200px' } = this.props
+    const {
+        getRootProps,
+        getInputProps,
+        isDragActive,
+    } = useDropzone({
+        accept: acceptedFileTypes,
+        onDrop: handleFilesDrop
+    })
 
-        return <ReactDropzone
-            accept={acceptedFileTypes}
-            onDrop={this.handleFilesDrop}
-            style={{width, height}}>
-            {({ getRootProps, getInputProps }) => (
-                <div {...getRootProps({className: 'dropzone'})} style={{width, height}}>
-                    <input {...getInputProps()} />
-                    {fileToBeImportedPreview ?
-                        <p className="fileDroppedMessage">
-                            <span className="checked large" />{fileToBeImportedPreview}
-                        </p>
-                        : <div className="fileSelectContainer">
-                            <p>{L.l('forms.dropzone.selectFile')}</p>
-                            <br/>
-                            <br/>
-                            <p>{L.l('forms.dropzone.supportedFileTypes', [acceptedFileTypesDescription])}</p>
-                        </div>
-                    }
-                </div>
-            )}
-        </ReactDropzone>
-    }
+    const className = `dropzone${isDragActive ? ' active' : ''}`
+
+    return <div {...getRootProps({isDragActive, className, style: { width, height } })}>
+        <input {...getInputProps()} />
+        {fileToBeImportedPreview ?
+            <p className="fileDroppedMessage">
+                <span className="checked large" />{fileToBeImportedPreview}
+            </p>
+            : <div className="fileSelectContainer">
+                <p>{L.l('forms.dropzone.selectFile')}</p>
+                <br />
+                <br />
+                <p>{L.l('forms.dropzone.supportedFileTypes', [acceptedFileTypesDescription])}</p>
+            </div>
+        }
+    </div>
 }
+
+export default Dropzone
