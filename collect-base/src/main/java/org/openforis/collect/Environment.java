91782d4c8af048e305f2792a632da28b85256c6c
Stefano Ricci <SteRiccio@users.noreply.github.com>
2020-12-18 18:29:44 +0100

    Fixed websocket configuration

--- collect-base/src/main/java/org/openforis/collect/Environment.java
+++ collect-base/src/main/java/org/openforis/collect/Environment.java
@@ -7,4 +7,21 @@ public class Environment {
 	public static boolean isAndroid() {
 		return MOBILE_VM_VENDOR.equalsIgnoreCase(System.getProperty("java.vendor"));
 	}
+
+	public static boolean isServerJetty() {
+		return classExists("org.eclipse.jetty.server.Server");
+	}
+
+	public static boolean isServerTomcat() {
+		return classExists("org.apache.catalina.startup.Tomcat");
+	}
+
+	private static boolean classExists(final String className) {
+		try {
+			Class.forName(className);
+			return true;
+		} catch (final ClassNotFoundException cnfe) {
+			return false;
+		}
+	}
 }
