8d89eba1914199e4148dbdb219e1a0c7e41ba37c
Stefano Ricci <SteRiccio@users.noreply.github.com>
2020-12-18 15:22:17 +0100

    Use latest version of sockjs

--- collect-server/src/main/java/org/openforis/collect/config/WebSocketConfig.java
+++ collect-server/src/main/java/org/openforis/collect/config/WebSocketConfig.java
@@ -10,12 +10,14 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
+	private static final String SOCK_JS_VERSION = "1.5.0";
+
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
 		super.configureMessageBroker(registry);
 		registry.setPreservePublishOrder(true);
 	}
-	
+
 	/**
 	 * Register Stomp endpoints: the url to open the WebSocket connection.
 	 */
@@ -25,7 +27,11 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 		// Register the "/ws" endpoint, enabling the SockJS protocol.
 		// SockJS is used (both client and server side) to allow alternative
 		// messaging options if WebSocket is not available.
-		registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS()
-				.setClientLibraryUrl("../assets/js/sockjs/1.3.0/sockjs.js");
+		registry.addEndpoint("/ws")
+//				.setHandshakeHandler(new DefaultHandshakeHandler(new JettyRequestUpgradeStrategy()))
+				.setAllowedOrigins("*")
+				.withSockJS()
+				.setClientLibraryUrl("../assets/js/sockjs/" + SOCK_JS_VERSION + "/sockjs.js")
+				;
 	}
 }
