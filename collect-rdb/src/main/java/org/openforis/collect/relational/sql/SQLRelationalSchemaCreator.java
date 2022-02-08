8b1157ff8005cbfd0c389737462c309f6f226687
Stefano Ricci <SteRiccio@users.noreply.github.com>
2022-02-08 23:06:08 +0100

    close resources in SQLRelationalSchemaCreator

--- collect-rdb/src/main/java/org/openforis/collect/relational/sql/SQLRelationalSchemaCreator.java
+++ collect-rdb/src/main/java/org/openforis/collect/relational/sql/SQLRelationalSchemaCreator.java
@@ -3,6 +3,7 @@ package org.openforis.collect.relational.sql;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import org.jooq.SQLDialect;
@@ -19,13 +20,20 @@ public class SQLRelationalSchemaCreator implements RelationalSchemaCreator {
 		RdbDialect rdbDialect = getRdbDialect(dsl);
 		Writer writer = new StringWriter();
 		SqlSchemaWriter schemaWriter = new SqlSchemaWriter(writer, schema, rdbDialect);
+		Statement stmt = null;
 		try {
 			schemaWriter.write();
 			String sql = writer.toString();
-			Statement stmt = conn.createStatement();
+			stmt = conn.createStatement();
 	        stmt.executeUpdate(sql);
 		} catch (Throwable e) {
 			throw new RuntimeException(String.format("Error generating schema on db for rdb schema %s", schema.getName()), e);
+		} finally {
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				// ignore it
+			}
 		}
 	}
 
