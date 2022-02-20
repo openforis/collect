cfcb87cf6b717cca854591eeb6da567eb962a92d
Stefano Ricci <SteRiccio@users.noreply.github.com>
2022-02-20 23:56:33 +0100

    code cleanup

---
+++ collect-server/src/main/java/org/openforis/collect/persistence/liquibase/migrations/SQLiteUtils.java
@@ -0,0 +1,25 @@
+package org.openforis.collect.persistence.liquibase.migrations;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.openforis.commons.versioning.Version;
+
+public class SQLiteUtils {
+
+	public static Version readVersionFromDb(Connection c) {
+		String query = "SELECT \"version\" FROM ofc_application_info";
+		try (Statement stmt = c.createStatement()) {
+			ResultSet rs = stmt.executeQuery(query);
+			if (rs.next()) {
+				String versionString = rs.getString(1);
+				return new Version(versionString);
+			}
+		} catch (Exception e) {
+			// ignore it
+		}
+		return null;
+	}
+
+}
