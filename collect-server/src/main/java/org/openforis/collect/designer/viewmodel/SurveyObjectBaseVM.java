a01130bfa30b766ddd5c825fa0848fd3cce3e4ae
Stefano Ricci <SteRiccio@users.noreply.github.com>
2022-05-05 00:51:31 +0200

    layout adjustments (code lists)

--- collect-server/src/main/java/org/openforis/collect/designer/viewmodel/SurveyObjectBaseVM.java
+++ collect-server/src/main/java/org/openforis/collect/designer/viewmodel/SurveyObjectBaseVM.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Locale;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.text.WordUtils;
+import org.apache.commons.text.WordUtils;
 import org.openforis.collect.designer.form.FormObject;
 import org.openforis.collect.designer.util.MessageUtil;
 import org.openforis.collect.designer.util.MessageUtil.ConfirmHandler;
