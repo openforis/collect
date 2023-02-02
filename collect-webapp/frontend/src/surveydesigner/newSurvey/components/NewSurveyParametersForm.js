d8fa3d28d901caca2647477650d3bb142e789e31
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2023-02-02 18:37:39 +0100

    Fixes to Survey Designer (#132)
    * survey designer: hide dependent nodes section if not available
    
    * survey designer / relevant: updated UI and label
    
    * survey designer: removed "Qualifier" attribute from CE survey
    
    * survey designer: removed "Referenced key attribute" from CE survey
    
    * survey designer: removed unused labels from CE survey
    
    * survey designer: make forms more compact
    
    * survey designer: changed text attribute type labels
    
    * survey creation: let the user select template and language
    
    * new survey: added template description
    
    * survey designer: enlarged default values and checks tables
    
    * layout adjustments
    
    * new survey form: fixed main languages
    
    * layout adjustments
    
    * survey edit: changed button icons
    
    ---------
    
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>

--- collect-webapp/frontend/src/surveydesigner/newSurvey/components/NewSurveyParametersForm.js
+++ collect-webapp/frontend/src/surveydesigner/newSurvey/components/NewSurveyParametersForm.js
@@ -1,100 +1,144 @@
+import './NewSurveyParametersForm.css'
+
 import React from 'react'
-import { compose } from "redux"
+import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { withFormik } from 'formik'
-import { Form, Row, Col } from 'reactstrap'
+import { Form, Row, Col, FormGroup, Label } from 'reactstrap'
 
 import User from 'model/User'
 
-import { TextFormItem, SelectFormItem, SubmitButton, normalizeInternalName, asyncValidate } from 'common/components/Forms'
+import {
+  TextFormItem,
+  SelectFormItem,
+  SubmitButton,
+  normalizeInternalName,
+  asyncValidate,
+} from 'common/components/Forms'
 import { createNewSurvey } from 'surveydesigner/newSurvey/actions'
 import ServiceFactory from 'services/ServiceFactory'
 import L from 'utils/Labels'
 import Languages from 'utils/Languages'
 import Strings from 'utils/Strings'
+import { Alert } from '@mui/material'
 
 const templateTypes = ['BLANK', 'BIOPHYSICAL', 'COLLECT_EARTH', 'COLLECT_EARTH_IPCC']
 
-const NewSurveyParametersForm = props => {
+const labelColSpan = 3
+const fieldColSpan = 6
+const fieldProps = { labelColSpan, fieldColSpan }
 
-    const {
-        userGroups,
-        handleSubmit,
-        handleChange,
-    } = props
+const NewSurveyParametersForm = (props) => {
+  const { userGroups, handleSubmit, handleChange, values } = props
 
-    const templateTypeOptions = templateTypes.map(type =>
-        <option key={type} value={type}>{L.l('survey.templateType.' + type)}</option>)
+  const EMPTY_OPTION = (
+    <option key="-1" value="" hidden>
+      {L.l('forms.selectOne')}
+    </option>
+  )
 
-    const userGroupOptions = [<option key="-1" value="">{L.l('forms.selectOne')}</option>].concat(
-        userGroups.map(g => <option key={g.id} value={g.id}>{g.label}</option>))
+  const templateTypeOptions = [EMPTY_OPTION].concat(
+    templateTypes.map((type) => (
+      <option key={type} value={type}>
+        {L.l('survey.templateType.' + type)}
+      </option>
+    ))
+  )
 
-    const languageOptions = Languages.items().sort((a, b) => Strings.compare(a.label, b.label)).map(item =>
-        <option key={item.code} value={item.code}>{item.label + ' (' + item.code + ')'}</option>)
+  const userGroupOptions = [EMPTY_OPTION].concat(
+    userGroups.map((g) => (
+      <option key={g.id} value={g.id}>
+        {g.label}
+      </option>
+    ))
+  )
 
-    const fieldProps = { labelColSpan: 4, fieldColSpan: 8 }
-    return (
-        <Form onSubmit={handleSubmit}>
-            <TextFormItem
-                name="name"
-                label={L.l('survey.name')}
-                {...fieldProps}
-                {...props}
-                handleChange={e => {
-                    e.target.value = normalizeInternalName(e.target.value)
-                    handleChange(e)
-                }}
-            />
-            <SelectFormItem
-                name="templateType"
-                label={L.l('survey.templateType')}
-                {...fieldProps}
-                {...props}
-            >{templateTypeOptions}</SelectFormItem>
-            <SelectFormItem
-                name="defaultLanguageCode"
-                label={L.l('survey.defaultLanguage')}
-                {...fieldProps}
-                {...props}
-            >{languageOptions}</SelectFormItem>
-            <SelectFormItem
-                name="userGroupId"
-                label={L.l('survey.userGroup')}
-                {...fieldProps}
-                {...props}
-            >{userGroupOptions}</SelectFormItem>
-            <Row>
-                <Col sm={{ size: 1, offset: 5 }}>
-                    <SubmitButton {...props}>{L.l('common.new')}</SubmitButton>
-                </Col>
-            </Row>
-        </Form>
-    )
+  const languageItems = Languages.items()
+  const mainLanguageCodes = ['ar', 'zh', 'en', 'fr', 'pt', 'ru', 'es']
+  const mainLanguageItems = mainLanguageCodes.map((code) => ({ code, label: Languages.label(code) }))
+  const otherLanguageItems = languageItems
+    .filter((item) => !mainLanguageCodes.includes(item.code))
+    .sort((a, b) => Strings.compare(a.label, b.label))
+
+  const languageItemToOption = (item) => (
+    <option key={item.code} value={item.code}>
+      {item.label + ' (' + item.code + ')'}
+    </option>
+  )
+
+  const languageOptions = [EMPTY_OPTION]
+    .concat(mainLanguageItems.map(languageItemToOption))
+    .concat([
+      <option key="__separator__" disabled>
+        ──────────
+      </option>,
+    ])
+    .concat(otherLanguageItems.map(languageItemToOption))
+
+  return (
+    <Form className="new-survey-form" onSubmit={handleSubmit}>
+      <TextFormItem
+        name="name"
+        label={L.l('survey.name')}
+        {...fieldProps}
+        fieldColSpan={9}
+        {...props}
+        handleChange={(e) => {
+          e.target.value = normalizeInternalName(e.target.value)
+          handleChange(e)
+        }}
+      />
+      <SelectFormItem name="templateType" label={L.l('survey.templateType')} {...fieldProps} {...props}>
+        {templateTypeOptions}
+      </SelectFormItem>
+      {values.templateType && (
+        <FormGroup row>
+          <Label sm={3}></Label>
+          <Col sm={9}>
+            <Alert className="new-survey-form__template-description" severity="info">
+              {L.l('survey.templateTypeDescription.' + values.templateType)}
+            </Alert>
+          </Col>
+        </FormGroup>
+      )}
+      <SelectFormItem name="defaultLanguageCode" label={L.l('survey.defaultLanguage')} {...fieldProps} {...props}>
+        {languageOptions}
+      </SelectFormItem>
+      <SelectFormItem name="userGroupId" label={L.l('survey.userGroup')} {...fieldProps} {...props}>
+        {userGroupOptions}
+      </SelectFormItem>
+      <Row>
+        <Col sm={{ size: 1, offset: 5 }}>
+          <SubmitButton {...props}>{L.l('common.new')}</SubmitButton>
+        </Col>
+      </Row>
+    </Form>
+  )
 }
 
-const mapStateToProps = state => {
-    const { items: userGroups } = state.userGroups
+const mapStateToProps = (state) => {
+  const { items: userGroups } = state.userGroups
 
-    return {
-        userGroups,
-    }
+  return {
+    userGroups,
+  }
 }
 
-const mapPropsToValues = props => {
-    const defaultPublicGroup = props.userGroups.find(ug => ug.name === User.DEFAULT_PUBLIC_GROUP_NAME)
-    return {
-        name: '',
-        templateType: 'BLANK',
-        defaultLanguageCode: 'en',
-        userGroupId: defaultPublicGroup ? defaultPublicGroup.id : ''
-    }
+const mapPropsToValues = (props) => {
+  const defaultPublicGroup = props.userGroups.find((ug) => ug.name === User.DEFAULT_PUBLIC_GROUP_NAME)
+  return {
+    name: '',
+    templateType: '',
+    defaultLanguageCode: '',
+    userGroupId: defaultPublicGroup ? defaultPublicGroup.id : '',
+  }
 }
 
 export default compose(
-    connect(mapStateToProps, { createNewSurvey }),
-    withFormik({
-        mapPropsToValues,
-        validate: asyncValidate(ServiceFactory.surveyService.validateSurveyCreation.bind(ServiceFactory.surveyService)),
-        handleSubmit: (values, { props }) => props.createNewSurvey(values)
-    })
+  connect(mapStateToProps, { createNewSurvey }),
+  withFormik({
+    mapPropsToValues,
+    validate: asyncValidate(ServiceFactory.surveyService.validateSurveyCreation.bind(ServiceFactory.surveyService)),
+    handleSubmit: (values, { props }) => props.createNewSurvey(values),
+  })
 )(NewSurveyParametersForm)
