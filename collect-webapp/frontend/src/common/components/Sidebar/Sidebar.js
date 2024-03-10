2fdde5a316dc1aadc25cc5f9030bad8072766a5d
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-03-10 23:21:30 +0100

    updated copyright year

--- collect-webapp/frontend/src/common/components/Sidebar/Sidebar.js
+++ collect-webapp/frontend/src/common/components/Sidebar/Sidebar.js
@@ -167,7 +167,7 @@ const Sidebar = () => {
             <a href="https://www.openforis.org" target="_blank" rel="noopener noreferrer">
               Open Foris
             </a>
-            &copy; 2010-2023
+            &copy; 2010-2024
           </span>
         </div>
       </nav>
