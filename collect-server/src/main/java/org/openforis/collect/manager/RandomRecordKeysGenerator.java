e33be5646ef41b8c8dcea1d8fdb9da215074047e
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-07-23 12:10:48 +0200

    code cleanup

--- collect-server/src/main/java/org/openforis/collect/manager/RandomRecordKeysGenerator.java
+++ collect-server/src/main/java/org/openforis/collect/manager/RandomRecordKeysGenerator.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import org.openforis.collect.metamodel.CollectAnnotations;
 import org.openforis.collect.model.CollectRecordSummary;
 import org.openforis.collect.model.CollectSurvey;
 import org.openforis.collect.model.RecordFilter;
@@ -18,24 +17,31 @@ public class RandomRecordKeysGenerator {
 	@Autowired
 	private transient RecordManager recordManager;
 
-	private AttributeDefinition getFirstMeasurementKeyDef(CollectSurvey survey) {
-		CollectAnnotations annotations = new CollectAnnotations(survey);
-		List<AttributeDefinition> keyDefs = survey.getSchema().getFirstRootEntityDefinition()
-				.getKeyAttributeDefinitions();
-		List<AttributeDefinition> measurementKeyDefs = new ArrayList<AttributeDefinition>(keyDefs);
-		CollectionUtils.filter(measurementKeyDefs, new Predicate<AttributeDefinition>() {
-			public boolean evaluate(AttributeDefinition keyDef) {
-				return annotations.isMeasurementAttribute(keyDef);
-			}
-		});
-		return measurementKeyDefs.isEmpty() ? null : measurementKeyDefs.get(0);
+	private static <T> List<T> generateRandomSubset(List<T> collection, float percentageOverTotal) {
+		List<T> result = new ArrayList<T>();
+		int numberOfRecordsToGenerate = (int) Math.ceil(((collection.size() * percentageOverTotal) / 100));
+		List<T> currentCollection = new ArrayList<T>(collection);
+		for (int i = 0; i < numberOfRecordsToGenerate; i++) {
+			int existingRecordIndex = (int) Math.round(Math.random() * currentCollection.size());
+			T item = currentCollection.get(existingRecordIndex);
+			result.add(item);
+			currentCollection.remove(existingRecordIndex);
+		}
+		return result;
+	}
+	
+	private static <T> List<T> transformList(List<T> list, Transformer<T> transformer) {
+		List<T> result = new ArrayList<T>();
+		for (T item : list) {
+			T itemTransformed = transformer.transform(item);
+			result.add(itemTransformed);
+		}
+		return result;
 	}
 
-	public List<List<String>> generate(CollectSurvey survey, String currentMeasurementValue, String nextMeasurementValue, float percentage) {
-		AttributeDefinition measurementKeyDef = getFirstMeasurementKeyDef(survey);
+	public List<List<String>> generate(CollectSurvey survey, String currentMeasurementValue, String nextMeasurementValue, float percentageOverTotal) {
+		AttributeDefinition measurementKeyDef = survey.getFirstMeasurementKeyDef();
 		if (measurementKeyDef == null) return Collections.emptyList();
-		
-		List<List<String>> result = new ArrayList<List<String>>();
 		
 		List<AttributeDefinition> keyDefs = survey.getSchema().getFirstRootEntityDefinition().getKeyAttributeDefinitions();
 		int measurementKeyDefIndex = keyDefs.indexOf(measurementKeyDef);
@@ -48,17 +54,23 @@ public class RandomRecordKeysGenerator {
 				return currentMeasurementValue.equals(measurementKeyValue);
 			};
 		});
-		
-		int numberOfRecordsToGenerate = (int) Math.ceil(((recordSummaries.size() * percentage) / 100));
-		for (int i = 0; i < numberOfRecordsToGenerate; i++) {
-			int existingRecordIndex = (int) Math.round(Math.random() * recordSummaries.size());
-			CollectRecordSummary existingRecordSummary = recordSummaries.get(existingRecordIndex);
-			List<String> keyValues = existingRecordSummary.getRootEntityKeyValues();
-			List<String> newKeyValues = new ArrayList<String>(keyValues);
-			newKeyValues.set(measurementKeyDefIndex, nextMeasurementValue);
-			result.add(newKeyValues);
-			recordSummaries.remove(existingRecordIndex);
+		List<List<String>> recordsKeys = new ArrayList<List<String>>();
+		for (CollectRecordSummary recordSummary : recordSummaries) {
+			recordsKeys.add(recordSummary.getRootEntityKeyValues());
 		}
-		return result;
+		List<List<String>> randomRecordsKeys = generateRandomSubset(recordsKeys, percentageOverTotal);
+		List<List<String>> transformedRandomRecordsKeys = transformList(randomRecordsKeys, new Transformer<List<String>>() {
+			public List<String> transform(List<String> keyValues) {
+				List<String> newKeyValues = new ArrayList<String>(keyValues);
+				newKeyValues.set(measurementKeyDefIndex, nextMeasurementValue);
+				return newKeyValues;
+			}
+		});
+		return transformedRandomRecordsKeys;
 	}
+	
+	private interface Transformer<T> {
+		T transform(T item);
+	}
+	
 }
