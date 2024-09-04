303e836567e3b70f343cb83194efe10ad4a3c438
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-09-04 22:09:27 +0200

    code cleanup

--- collect-webapp/frontend/src/datamanagement/pages/CsvDataExportPage.js
+++ collect-webapp/frontend/src/datamanagement/pages/CsvDataExportPage.js
@@ -110,7 +110,7 @@ class CsvDataExportPage extends Component {
     const surveyId = survey.id
 
     const keyAttributeValues = keyAttributes.map((_a, idx) => this.state['key' + idx], this)
-    const summaryAttributeValues = summaryAttributes.map((a, idx) => this.state['summary' + idx], this)
+    const summaryAttributeValues = summaryAttributes.map((_a, idx) => this.state['summary' + idx], this)
 
     const selectedEntityId =
       exportMode === exportModes.allEntities ? null : selectedEntityDefinition ? selectedEntityDefinition.id : null
